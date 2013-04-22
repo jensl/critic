891de2d9ce7db1b5ac870250ea81382347e12bee
Jens Lindstrom <jl@opera.com>
2013-04-22 20:03:09 +0200

    Add basic "create review, then push commit to it" test
    This test catches the regression that 650cf3d1 fixed, and gives some
    amount of code at least basic testing.

---
+++ testing/tests/001-main/003-self/004-createreview.py
@@ -0,0 +1,147 @@
+# Scenario: Alice creates a review of a single commit with review filters that
+# make Bob a reviewer and Dave a watcher, and then pushes a second commit to
+# that review.
+#
+# Checks: Mostly that this doesn't fail completely, and that the expected mails
+# appear to be sent.
+
+import re
+
+# Random commit on master:
+COMMIT_SHA1 = "f771149aba230c4712c9cb9c6af4ccfea2b7967d"
+COMMIT_SUMMARY = "Minor /dashboard query optimizations"
+
+# The next commit on master:
+FOLLOWUP_SHA1 = "e0892183f38932cec0d33408bdfebb290a13f8f3"
+
+def check_summary_input(document):
+    input = document.find("input", attrs={ "id": "summary" })
+
+    if not input:
+        testing.expect.check("<review summary input>",
+                             "<expected content not found>")
+
+    testing.expect.check(COMMIT_SUMMARY, input["value"])
+
+with frontend.signin("alice"):
+    # Loading /createreview is not really necessary, but might as well try that
+    # as well.
+    document = frontend.page(
+        "createreview",
+        params={ "repository": "critic",
+                 "commits": COMMIT_SHA1 },
+        expect={ "document_title": testing.expect.document_title(u"Create Review"),
+                 "summary_input": check_summary_input })
+
+    scripts = document.findAll("script")
+
+    for script in scripts:
+        if script.has_key("src"):
+            continue
+        match = re.search(
+            r"^\s*var review\s*=\s*\{\s*commit_ids:\s*\[\s*(\d+)\s*\]",
+            script.string, re.MULTILINE)
+        if match:
+            commit_id = int(match.group(1))
+            break
+    else:
+        testing.expect.check("<data script>",
+                             "<expected content not found>")
+
+    result = frontend.operation(
+        "submitreview",
+        data={ "repository_id": 1,
+               "commit_ids": [commit_id],
+               "branch": "r/004-createreview",
+               "summary": COMMIT_SUMMARY,
+               "applyfilters": True,
+               "applyparentfilters": True,
+               "reviewfilters": [{ "username": "bob",
+                                   "type": "reviewer",
+                                   "path": "/" },
+                                 { "username": "dave",
+                                   "type": "watcher",
+                                   "path": "/" }],
+               "recipientfilters": { "mode": "opt-out" }},
+        expect={ "review_id": 1 })
+
+    def to(name):
+        return testing.mailbox.to_recipient("%s@example.org" % name)
+
+    def check_initial(mail):
+        testing.expect.check("New Review: %s" % COMMIT_SUMMARY,
+                             mail.header("Subject"))
+        line = "Commit: %s" % COMMIT_SHA1
+        if line not in to_alice.lines:
+            testing.expect.check("<%r line>" % line,
+                                 "<expected content not found>")
+
+    to_alice = mailbox.pop(accept=to("alice"), timeout=30)
+    if to_alice:
+        check_initial(to_alice)
+        testing.expect.check("owner",
+                             to_alice.header("OperaCritic-Association"))
+    else:
+        testing.expect.check("<mail to alice>",
+                             "<expected mail not received>")
+
+    to_bob = mailbox.pop(accept=to("bob"), timeout=30)
+    if to_bob:
+        check_initial(to_bob)
+        testing.expect.check("reviewer",
+                             to_bob.header("OperaCritic-Association"))
+    else:
+        testing.expect.check("<mail to bob>",
+                             "<expected mail not received>")
+
+    to_dave = mailbox.pop(accept=to("dave"), timeout=30)
+    if to_dave:
+        check_initial(to_dave)
+        testing.expect.check("watcher",
+                             to_dave.header("OperaCritic-Association"))
+    else:
+        testing.expect.check("<mail to dave>",
+                             "<expected mail not received>")
+
+    mailbox.check_empty()
+
+    with repository.workcopy() as work:
+        work.run(["checkout", "-q", "-b", "r/004-createreview", COMMIT_SHA1])
+        work.run(["cherry-pick", FOLLOWUP_SHA1])
+
+        followup_sha1 = work.run(["rev-parse", "HEAD"]).strip()
+
+        work.run([
+            "push", "-q", "alice@%s:/var/git/critic.git" % instance.hostname,
+            "HEAD:refs/heads/r/004-createreview"])
+
+    def check_followup(mail):
+        testing.expect.check("Updated Review: %s" % COMMIT_SUMMARY,
+                             mail.header("Subject"))
+        line = "Commit: %s" % followup_sha1
+        if line not in mail.lines:
+            testing.expect.check("<%r line>" % line,
+                                 "<expected content not found>")
+
+    to_alice = mailbox.pop(accept=to("alice"), timeout=30)
+    if to_alice:
+        check_followup(to_alice)
+    else:
+        testing.expect.check("<mail to alice>",
+                             "<expected mail not received>")
+
+    to_bob = mailbox.pop(accept=to("bob"), timeout=30)
+    if to_bob:
+        check_followup(to_bob)
+    else:
+        testing.expect.check("<mail to bob>",
+                             "<expected mail not received>")
+
+    to_dave = mailbox.pop(accept=to("dave"), timeout=30)
+    if to_dave:
+        check_followup(to_dave)
+    else:
+        testing.expect.check("<mail to dave>",
+                             "<expected mail not received>")
+
+    mailbox.check_empty()
