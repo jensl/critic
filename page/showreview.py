jensl/critic

commit 38667dc87a03397d3cdbd39d1bfba986b3a41a74

Rewrite filter system
Primarily adding support for wild-card filters: "**" matching zero or
more complete path components (only allowed at the beginning of a path
or between path separators,) "*" matching zero or more characters
except the path separator and "?" matching one character except the
path separator.

Also adding support for a third filter type, "ignored," which negates
less specific reviewer or watcher filters and makes it possible to
ignore changes in specific files or sub-trees.  Since the migration
script uses "ALTER TYPE enumtype ADD VALUE value" to perform this
change, which requires PostgreSQL 9.1, I changed the installation
prerequisites check to strictly require PostgreSQL 9.1 or later.  The
migration script doesn't check, but will fail in older PostgreSQL
versions.  This version of PostgreSQL is included in Ubuntu 12.04 and
12.10, as well as in Debian Wheezy, and Debian Squeeze, which has
PostgreSQL 8.4, is already not supported due to a too old Python.

Also revamping the filters UI on /home to display all filters in all
repositories at once, and to display helpful information such as the
number of files matched by a filter, and even list those files.  The
UI for adding a filter is now a dialog with auto-completion for the
path.  The documentation for the filters mechanism is moved from its
rather odd location on /home to a tutorial.  (The documentation on
/home predated the whole tutorial mechanism.  It predated almost the
entire system, in fact.)

Related and significant but under-the-hood change: reduce the 'files'
table to a simple path=>id mapping (where the path is of type TEXT,
i.e. "arbitrarily long string") and drop the directories table
altogether.  This makes a lot of things both simpler and faster.  The
only real problem with this is that PostgreSQL has an internal limit
on the size of "index rows," which means the new 'path' column can't
be indexed directly, and thus also can't be part of a primary key or

page/showreview.py
@@ -34,6 +34,8 @@ import profiling
 import linkify
 import extensions
 
+from textutils import json_encode
+
 try:
     from customization.paths import getModuleFromFile
 except:
@@ -410,7 +412,7 @@ def renderShowReview(req, db, user):
         else:
             target.i().text("No reviewers.")
 
-        cursor.execute("""SELECT reviewfilters.id, reviewfilters.uid, reviewfilters.directory, reviewfilters.file
+        cursor.execute("""SELECT reviewfilters.id, reviewfilters.uid, reviewfilters.path
                             FROM reviewfilters
                             JOIN users ON (reviewfilters.uid=users.id)
                            WHERE reviewfilters.review=%s
@@ -430,13 +432,9 @@ def renderShowReview(req, db, user):
             filter_data = {}
             reviewfilters = {}
 
-            for filter_id, user_id, directory_id, file_id in rows:
+            for filter_id, user_id, path in rows:
                 filter_user = dbutils.User.fromId(db, user_id)
-
-                if file_id: path = dbutils.describe_file(db, file_id)
-                else: path = dbutils.describe_directory(db, directory_id) + "/"
-
-                reviewfilters.setdefault(filter_user.fullname, []).append(path)
+                reviewfilters.setdefault(filter_user.fullname, []).append(path or "/")
                 filter_data[(filter_user.fullname, path)] = (filter_id, filter_user)
 
             count = 0
@@ -499,7 +497,7 @@ def renderShowReview(req, db, user):
         else:
             target.i().text("No watchers.")
 
-        cursor.execute("""SELECT reviewfilters.id, reviewfilters.uid, reviewfilters.directory, reviewfilters.file
+        cursor.execute("""SELECT reviewfilters.id, reviewfilters.uid, reviewfilters.path
                             FROM reviewfilters
                             JOIN users ON (reviewfilters.uid=users.id)
                            WHERE reviewfilters.review=%s
@@ -519,13 +517,9 @@ def renderShowReview(req, db, user):
             filter_data = {}
             reviewfilters = {}
 
-            for filter_id, user_id, directory_id, file_id in rows:
+            for filter_id, user_id, path in rows:
                 filter_user = dbutils.User.fromId(db, user_id)
-
-                if file_id: path = dbutils.describe_file(db, file_id)
-                else: path = dbutils.describe_directory(db, directory_id) + "/"
-
-                reviewfilters.setdefault(filter_user.fullname, []).append(path)
+                reviewfilters.setdefault(filter_user.fullname, []).append(path or "/")
                 filter_data[(filter_user.fullname, path)] = (filter_id, filter_user)
 
             count = 0
@@ -1039,13 +1033,12 @@ def renderShowReview(req, db, user):
             for path in diff.File.eliminateCommonPrefixes(sorted(modules)):
                 cell.span("file").innerHTML(path).br()
 
-            directory_ids = "[ %s ]" % ", ".join([str(dbutils.find_directory(db, path=path[:-1])) for path in modules if path.endswith("/")])
-            file_ids = "[ %s ]" % ", ".join([str(dbutils.find_file(db, path=path)) for path in modules if not path.endswith("/")])
-            user_ids = "[ %s ]" % ", ".join(map(str, team))
+            paths = json_encode(list(modules))
+            user_ids = json_encode(list(team))
 
             cell = row.td("buttons")
-            cell.button("accept", critic_directory_ids=directory_ids, critic_file_ids=file_ids, critic_user_ids=user_ids).text("I will review this!")
-            cell.button("deny", critic_directory_ids=directory_ids, critic_file_ids=file_ids, critic_user_ids=user_ids).text("They will review this!")
+            cell.button("accept", critic_paths=paths, critic_user_ids=user_ids).text("I will review this!")
+            cell.button("deny", critic_paths=paths, critic_user_ids=user_ids).text("They will review this!")
 
     yield flush(target)
 

INSTALL
@@ -29,7 +29,7 @@ Required Software Packages
 
 Critic depends on the following software packages:
 
-  * PostgreSQL (8.4 or later)
+  * PostgreSQL (9.1 or later)
 
   * Apache (2.2 or later; 2.4 hasn't been tested)
 

changeset/load.py
@@ -47,15 +47,17 @@ def loadChangesets(db, repository, changesets, filtered_file_ids=None, load_chun
     filtered_file_ids = list(filtered_file_ids) if filtered_file_ids else None
 
     if filtered_file_ids is None:
-        cursor.execute("""SELECT changeset, file, fullfilename(file), old_sha1, new_sha1, old_mode, new_mode
+        cursor.execute("""SELECT changeset, file, path, old_sha1, new_sha1, old_mode, new_mode
                             FROM fileversions
-                            WHERE changeset=ANY (%s)""",
+                            JOIN files ON (files.id=fileversions.file)
+                           WHERE changeset=ANY (%s)""",
                        (changeset_ids,))
     else:
-        cursor.execute("""SELECT changeset, file, fullfilename(file), old_sha1, new_sha1, old_mode, new_mode
+        cursor.execute("""SELECT changeset, file, path, old_sha1, new_sha1, old_mode, new_mode
                             FROM fileversions
-                            WHERE changeset=ANY (%s)
-                              AND file=ANY (%s)""",
+                            JOIN files ON (files.id=fileversions.file)
+                           WHERE changeset=ANY (%s)
+                             AND file=ANY (%s)""",
                        (changeset_ids, filtered_file_ids))
 
     files = dict([(changeset.id, {}) for changeset in changesets])

critic.py
@@ -185,164 +185,6 @@ def setfullname(req, db, user):
 
     return "ok"
 
-def addfilter(req, db, user):
-    if user.isAnonymous(): return OperationFailureMustLogin()
-
-    cursor = db.cursor()
-
-    repository_id = req.getParameter("repository", filter=int)
-    filter_type = req.getParameter("type")
-    filter_path = req.getParameter("path")
-    filter_delegate = req.getParameter("delegate", "")
-    force = req.getParameter("force", "no") == "yes"
-
-    repository = gitutils.Repository.fromId(db, repository_id)
-
-    if filter_delegate:
-        invalid_users = []
-        for delegate_name in map(str.strip, filter_delegate.split(',')):
-            if dbutils.User.fromName(db, delegate_name) is None:
-                invalid_users.append(delegate_name)
-        if invalid_users: return "error:invalid-users:%s" % ','.join(invalid_users)
-
-    if filter_path == '/':
-        directory_id, file_id = 0, 0
-    elif filter_path[-1] == '/':
-        directory_id, file_id = dbutils.find_directory(db, path=filter_path[:-1]), 0
-    else:
-        if not force and dbutils.is_directory(db, filter_path): return "error:directory"
-        else: directory_id, file_id = dbutils.find_directory_file(db, filter_path)
-
-    if directory_id:
-        specificity = len(dbutils.explode_path(db, directory_id=directory_id))
-        if file_id: specificity += 1
-    else:
-        specificity = 0
-
-    cursor.execute("INSERT INTO filters (uid, repository, directory, file, specificity, type, delegate) VALUES (%s, %s, %s, %s, %s, %s, %s)", [user.id, repository.id, directory_id, file_id, specificity, filter_type, ','.join(map(str.strip, filter_delegate.split(',')))])
-    user.setPreference(db, "email.activated", True)
-
-    db.commit()
-
-    return "ok:directory=%d,file=%d" % (directory_id, file_id)
-
-def deletefilter(req, db, user):
-    if user.isAnonymous(): return OperationFailureMustLogin()
-
-    cursor = db.cursor()
-
-    repository_id = req.getParameter("repository", filter=int)
-    directory_id = req.getParameter("directory", filter=int)
-    file_id = req.getParameter("file", filter=int)
-
-    cursor.execute("DELETE FROM filters WHERE uid=%s AND repository=%s AND directory=%s AND file=%s", (user.id, repository_id, directory_id, file_id))
-
-    db.commit()
-
-    return "ok"
-
-def reapplyfilters(req, db, user):
-    if user.isAnonymous(): return OperationFailureMustLogin()
-
-    cursor1 = db.cursor()
-    cursor2 = db.cursor()
-    cursor3 = db.cursor()
-
-    user = dbutils.User.fromName(db, req.getParameter("user", req.user))
-    repository_name = req.getParameter("repository", None)
-
-    if not repository_name:
-        cursor1.execute("""SELECT reviews.id, applyfilters, applyparentfilters, branches.repository FROM reviews JOIN branches ON (reviews.branch=branches.id) WHERE reviews.state!='closed'""")
-    else:
-        repository = gitutils.Repository.fromParameter(db, repository_name)
-        cursor1.execute("""SELECT reviews.id, applyfilters, applyparentfilters, branches.repository FROM reviews JOIN branches ON (reviews.branch=branches.id) WHERE reviews.state!='closed' AND branches.repository=%s""", (repository.id,))
-
-    repositories = {}
-
-    assign_changes = {}
-    watch_reviews = set()
-
-    own_commit = {}
-
-    for review_id, applyfilters, applyparentfilters, repository_id in cursor1:
-        if repository_id not in repositories:
-            repositories[repository_id] = gitutils.Repository.fromId(db, repository_id)
-        repository = repositories[repository_id]
-
-        filters = review_filters.Filters()
-        filters.load(db, review=review_filters.Filters.Review(review_id, applyfilters, applyparentfilters, repository), user=user)
-
-        if filters.hasFilters():
-            cursor2.execute("""SELECT changesets.child, reviewfiles.file, reviewfiles.id
-                                 FROM changesets
-                                 JOIN reviewfiles ON (reviewfiles.changeset=changesets.id)
-                      LEFT OUTER JOIN reviewuserfiles ON (reviewuserfiles.file=reviewfiles.id
-                                                      AND reviewuserfiles.uid=%s)
-                                WHERE reviewfiles.review=%s
-                                  AND reviewuserfiles.uid IS NULL""",
-                            (user.id, review_id))
-
-            for commit_id, file_id, review_file_id in cursor2:
-                users = filters.listUsers(db, file_id)
-
-                if user.id in users:
-                    if commit_id not in own_commit:
-                        cursor3.execute("""SELECT uid
-                                             FROM usergitemails
-                                             JOIN gitusers USING (email)
-                                             JOIN commits ON (commits.author_gituser=gitusers.id)
-                                            WHERE commits.id=%s""",
-                                        (commit_id,))
-                        own_commit[commit_id] = cursor3.fetchone()[0] == user.id
-
-                    if not own_commit[commit_id]:
-                        if users[user.id][0] == 'reviewer':
-                            assign_changes.setdefault(review_id, set()).add((file_id, review_file_id))
-                        else:
-                            watch_reviews.add(review_id)
-
-    new_reviews = set()
-
-    for review_id in itertools.chain(assign_changes, watch_reviews):
-        cursor1.execute("SELECT 1 FROM reviewusers WHERE review=%s AND uid=%s", (review_id, user.id))
-        if not cursor1.fetchone(): new_reviews.add(review_id)
-
-    cursor1.executemany("INSERT INTO reviewusers (review, uid) VALUES (%s, %s)", [(review_id, user.id) for review_id in new_reviews])
-
-    reviewuserfiles_values = []
-
-    for file_ids in assign_changes.values():
-        reviewuserfiles_values.extend([(review_file_id, user.id) for file_id, review_file_id in file_ids])
-
-    cursor1.executemany("INSERT INTO reviewuserfiles (file, uid) VALUES (%s, %s)", reviewuserfiles_values)
-
-    result = ""
-
-    for review_id in sorted(assign_changes.keys()):
-        review = dbutils.Review.fromId(db, review_id, load_commits=False)
-        file_ids = assign_changes[review_id]
-
-        if review.state == 'open':
-            result += "review,%s:%d:%s\n" % ("new" if review_id in new_reviews else "old", review.id, review.summary)
-
-            paths = [dbutils.describe_file(db, file_id) for file_id, review_file_id in file_ids]
-
-            for path in diff.File.eliminateCommonPrefixes(sorted(paths), text=True):
-                result += "  " + path + "\n"
-
-    for review_id in sorted(watch_reviews & new_reviews):
-        review = dbutils.Review.fromId(db, review_id, load_commits=False)
-
-        if review.state == 'open':
-            result += "watch:%d:%s\n" % (review.id, review.summary)
-
-    db.commit()
-
-    if not result:
-        result = "nothing\n"
-
-    return result
-
 def savesettings(req, db, user):
     if user.isAnonymous():
         return "ok"
@@ -387,28 +229,22 @@ def showfilters(req, db, user):
 
     path = path.rstrip("/")
 
-    if dbutils.is_directory(db, path):
-        directory_id = dbutils.find_directory(db, path=path)
+    if repository.getHead(db).isDirectory(path):
         show_path = path + "/"
-
-        cursor = db.cursor()
-        cursor.execute("SELECT name FROM files WHERE directory=%s ORDER BY id ASC LIMIT 1", (directory_id,))
-
-        row = cursor.fetchone()
-        if row: path += "/" + row[0]
-        else: path += "/dummy.txt"
+        path += "/dummy.txt"
     else:
         show_path = path
 
     file_id = dbutils.find_file(db, path=path)
 
     filters = review_filters.Filters()
+    filters.setFiles(db, [file_id])
     filters.load(db, repository=repository, recursive=True)
 
     reviewers = []
     watchers = []
 
-    for user_id, (filter_type, _delegate) in filters.listUsers(db, file_id).items():
+    for user_id, (filter_type, _delegate) in filters.listUsers(file_id).items():
         if filter_type == 'reviewer': reviewers.append(user_id)
         else: watchers.append(user_id)
 
@@ -618,9 +454,11 @@ OPERATIONS = { "fetchlines": operation.fetchlines.FetchLines(),
                "cancelrebase": operation.rebasereview.CancelRebase(),
                "rebasereview": operation.rebasereview.RebaseReview(),
                "revertrebase": operation.rebasereview.RevertRebase(),
-               "addfilter": addfilter,
-               "deletefilter": deletefilter,
-               "reapplyfilters": reapplyfilters,
+               "addfilter": operation.manipulatefilters.AddFilter(),
+               "deletefilter": operation.manipulatefilters.DeleteFilter(),
+               "reapplyfilters": operation.manipulatefilters.ReapplyFilters(),
+               "countmatchedpaths": operation.manipulatefilters.CountMatchedPaths(),
+               "getmatchedpaths": operation.manipulatefilters.GetMatchedPaths(),
                "markfiles": operation.markfiles.MarkFiles(),
                "submitchanges": operation.draftchanges.SubmitChanges(),
                "abortchanges": operation.draftchanges.AbortChanges(),
@@ -641,6 +479,7 @@ OPERATIONS = { "fetchlines": operation.fetchlines.FetchLines(),
                "addnewsitem": operation.news.AddNewsItem(),
                "editnewsitem": operation.news.EditNewsItem(),
                "getautocompletedata": operation.autocompletedata.GetAutoCompleteData(),
+               "getrepositorypaths": operation.autocompletedata.GetRepositoryPaths(),
                "addrecipientfilter": operation.recipientfilter.AddRecipientFilter(),
                "trackedbranchlog": operation.trackedbranch.TrackedBranchLog(),
                "disabletrackedbranch": operation.trackedbranch.DisableTrackedBranch(),
@@ -819,7 +658,9 @@ def main(environ, start_response):
                     req.setContentType("text/json")
 
                     if isinstance(result, OperationResult):
-                        if db.profiling: result.set("__profiling__", formatDBProfiling(db))
+                        if db.profiling:
+                            result.set("__profiling__", formatDBProfiling(db))
+                            result.set("__time__", time.time() - request_start)
                         result.addResponseHeaders(req)
                 else:
                     req.setContentType("text/plain")

dbschema.sql
@@ -131,43 +131,32 @@ CREATE TABLE trackedbranchlog
     successful BOOLEAN NOT NULL );
 CREATE INDEX trackedbranchlog_branch ON trackedbranchlog (branch);
 
-CREATE TABLE directories
-  ( id SERIAL PRIMARY KEY,
-    directory INTEGER NOT NULL,
-    name VARCHAR(256) NOT NULL,
-
-    UNIQUE (directory, name) );
-CREATE INDEX directories_directory_name ON directories (directory, name);
-
 CREATE TABLE files
   ( id SERIAL PRIMARY KEY,
-    directory INTEGER NOT NULL,
-    name VARCHAR(256) NOT NULL,
+    path TEXT NOT NULL );
 
-    UNIQUE (directory, name) );
-CREATE INDEX files_directory_name ON files (directory, name);
+-- Index used to enforce uniqueness, and for quick lookup of single
+-- paths (using "SELECT id FROM files WHERE MD5(path)=MD5(...)".
+CREATE UNIQUE INDEX files_path_md5 ON files (MD5(path));
+
+-- Index used for path searches, for instance when searching for
+-- reviews that touch files in a certain directory.
+CREATE INDEX files_path_gin ON files USING gin (STRING_TO_ARRAY(path, '/'));
 
 CREATE TYPE filtertype AS ENUM
   ( 'reviewer',
-    'watcher' );
+    'watcher',
+    'ignored' );
 CREATE TABLE filters
   ( id SERIAL PRIMARY KEY,
     uid INTEGER NOT NULL REFERENCES users,
     repository INTEGER NOT NULL REFERENCES repositories ON DELETE CASCADE,
-    directory INTEGER NOT NULL,
-    file INTEGER NOT NULL DEFAULT 0,
-    specificity INTEGER NOT NULL,
+    path TEXT NOT NULL,
     type filtertype NOT NULL,
-    delegate TEXT,
+    delegate TEXT );
 
-    UNIQUE (uid, directory, file) );
-
-CREATE TABLE filteredfiles
-  ( filter INTEGER NOT NULL REFERENCES filters ON DELETE CASCADE,
-    repository INTEGER NOT NULL REFERENCES repositories ON DELETE CASCADE,
-    file INTEGER NOT NULL REFERENCES files ON DELETE CASCADE );
-CREATE INDEX filteredfiles_filter ON filteredfiles(filter);
-CREATE INDEX filteredfiles_file ON filteredfiles(file);
+-- Index used to enforce uniqueness.
+CREATE UNIQUE INDEX filters_uid_repository_path_md5 ON filters (uid, repository, MD5(path));
 
 CREATE TABLE commits
   ( id SERIAL PRIMARY KEY,
@@ -307,12 +296,12 @@ CREATE TABLE reviewfilters
 
     review INTEGER NOT NULL REFERENCES reviews ON DELETE CASCADE,
     uid INTEGER NOT NULL REFERENCES users ON DELETE CASCADE,
-    directory INTEGER NOT NULL,
-    file INTEGER NOT NULL DEFAULT 0,
+    path TEXT NOT NULL,
     type filtertype NOT NULL,
-    creator INTEGER NOT NULL REFERENCES users ON DELETE CASCADE,
+    creator INTEGER NOT NULL REFERENCES users ON DELETE CASCADE );
 
-    UNIQUE (review, uid, directory, file) );
+-- Index used to enforce uniqueness.
+CREATE UNIQUE INDEX reviewfilters_review_uid_path_md5 ON reviewfilters (review, uid, MD5(path));
 
 CREATE TABLE batches
   ( id SERIAL PRIMARY KEY,
@@ -404,8 +393,7 @@ CREATE TABLE reviewfilterchanges
   ( transaction INTEGER NOT NULL REFERENCES reviewassignmentstransactions ON DELETE CASCADE,
 
     uid INTEGER NOT NULL REFERENCES users ON DELETE CASCADE,
-    directory INTEGER NOT NULL,
-    file INTEGER NOT NULL DEFAULT 0,
+    path TEXT NOT NULL,
     type filtertype NOT NULL,
     created BOOLEAN NOT NULL );
 

dbutils/__init__.py
@@ -21,9 +21,7 @@ from dbutils.database import Database
 from dbutils.user import NoSuchUser, User
 from dbutils.review import ReviewState, Review
 from dbutils.branch import Branch
-from dbutils.paths import is_directory, find_directory, describe_directory, \
-                          is_file, find_file, find_files, describe_file, \
-                          find_directory_file, explode_path, contained_files
+from dbutils.paths import find_file, find_files, describe_file
 
 def getURLPrefix(db):
     import configuration

dbutils/database.py
@@ -99,6 +99,9 @@ class Database(Session):
                 after = time.time()
                 self.__db.recordProfiling(query, after - before, repetitions=len(params))
 
+        def mogrify(self, *args):
+            return self.__cursor.mogrify(*args)
+
     def __init__(self):
         super(Database, self).__init__()
         self.__connection = dbaccess.connect()

dbutils/paths.py
@@ -14,123 +14,29 @@
 # License for the specific language governing permissions and limitations under
 # the License.
 
-def find_directory(db, path):
-    path = path.strip("/")
-
-    cursor = db.cursor()
-    cursor.execute("SELECT finddirectory(%s)", (path,))
-
-    row = cursor.fetchone()
-
-    if row and row[0] is not None:
-        return row[0]
-
-    if "/" in path:
-        directory, name = path.rsplit("/", 1)
-        directory_id = find_directory(db, directory)
-    else:
-        directory_id, name = 0, path
-
-    cursor.execute("INSERT INTO directories (directory, name) VALUES (%s, %s) RETURNING id", (directory_id, name))
-    return cursor.fetchone()[0]
-
-def is_directory(db, path):
-    cursor = db.cursor()
-    cursor.execute("SELECT finddirectory(%s)", (path,))
-
-    directory_id = cursor.fetchone()[0]
-
-    if directory_id is None: return False
-
-    cursor.execute("SELECT 1 FROM files WHERE directory=%s LIMIT 1", (directory_id,))
-    if cursor.fetchone(): return True
-
-    cursor.execute("SELECT 1 FROM directories WHERE directory=%s LIMIT 1", (directory_id,))
-    if cursor.fetchone(): return True
-
-    return False
-
-def is_file(db, path):
-    cursor = db.cursor()
-    cursor.execute("SELECT findfile(%s)", (path,))
-
-    file_id = cursor.fetchone()[0]
-
-    if file_id is None: return False
-
-    cursor.execute("SELECT 1 FROM fileversions WHERE file=%s LIMIT 1", (file_id,))
-    if cursor.fetchone(): return True
-
-    return False
-
 def find_file(db, path):
     path = path.lstrip("/")
 
     assert not path.endswith("/")
 
     cursor = db.cursor()
-    cursor.execute("SELECT findfile(%s)", (path,))
+    cursor.execute("SELECT id, path FROM files WHERE MD5(path)=MD5(%s)", (path,))
 
     row = cursor.fetchone()
 
-    if row and row[0] is not None:
-        return row[0]
+    if row:
+        file_id, found_path = row
+        assert path == found_path, "MD5 collision in files table: %r != %r" % (path, found_path)
+        return file_id
 
-    if "/" in path:
-        directory, name = path.rsplit("/", 1)
-        directory_id = find_directory(db, directory)
-    else:
-        directory_id, name = 0, path
-
-    cursor.execute("INSERT INTO files (directory, name) VALUES (%s, %s) RETURNING id", (directory_id, name))
+    cursor.execute("INSERT INTO files (path) VALUES (%s) RETURNING id", (path,))
     return cursor.fetchone()[0]
 
 def find_files(db, files):
     for file in files:
-        file.id = find_file(db, path=file.path)
-
-def find_directory_file(db, path):
-    path = path.lstrip("/")
-
-    assert not path.endswith("/")
-
-    file_id = find_file(db, path)
-    if "/" in path:
-        directory_id = find_directory(db, path.rsplit("/", 1)[0])
-    else:
-        directory_id = 0
-    return directory_id, file_id
-
-def describe_directory(db, directory_id):
-    cursor = db.cursor()
-    cursor.execute("SELECT fulldirectoryname(%s)", (directory_id,))
-    return cursor.fetchone()[0].rstrip("/")
+        file.id = find_file(db, file.path)
 
 def describe_file(db, file_id):
     cursor = db.cursor()
-    cursor.execute("SELECT fullfilename(%s)", (file_id,))
+    cursor.execute("SELECT path FROM files WHERE id=%s", (file_id,))
     return cursor.fetchone()[0]
-
-def explode_path(db, invalid=None, file_id=None, directory_id=None):
-    assert invalid is None
-    assert (file_id is None) != (directory_id is None)
-
-    cursor = db.cursor()
-    path = []
-
-    if file_id is not None:
-        cursor.execute("SELECT * FROM filepath(%s)", (file_id,))
-    else:
-        path.append(directory_id)
-        if not directory_id: return path
-        cursor.execute("SELECT * FROM directorypath(%s)", (directory_id,))
-
-    for (directory_id,) in cursor:
-        path.insert(0, directory_id)
-
-    return path
-
-def contained_files(db, directory_id):
-    cursor = db.cursor()
-    cursor.execute("SELECT file_out FROM containedfiles(%s)", (directory_id,))
-    return [file_id for (file_id,) in cursor]

commit a663c6189a828b07e6e5985fcd43c60ba3ab4378

Fix crash on /showreview on review filters with empty path
Regression introduced by commit 38667dc "Rewrite filter system".

page/showreview.py
@@ -440,7 +440,8 @@ def renderShowReview(req, db, user):
 
             for filter_id, user_id, path in rows:
                 filter_user = dbutils.User.fromId(db, user_id)
-                reviewfilters.setdefault(filter_user.fullname, []).append(path or "/")
+                path = path or '/'
+                reviewfilters.setdefault(filter_user.fullname, []).append(path)
                 filter_data[(filter_user.fullname, path)] = (filter_id, filter_user)
 
             count = 0