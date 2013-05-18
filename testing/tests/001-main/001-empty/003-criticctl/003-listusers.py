2b603ea26205065f4c242a2a7188bd1d41559807
Jens Lindstrom <jl@opera.com>
2013-05-19 00:10:33 +0200

    Add test for 'criticctl listusers' functionality

---
+++ testing/tests/001-main/001-empty/003-criticctl/003-listusers.py
@@ -0,0 +1,110 @@
+# Scenario: Invalid format.
+try:
+    instance.execute(
+        ["sudo", "criticctl", "listusers", "--format", "oranges"])
+except testing.virtualbox.GuestCommandError as error:
+    if "invalid choice: 'oranges'" not in error.output:
+        logger.error("criticctl failed with unexpected error message:\n%s"
+                     % error.output)
+else:
+    logger.error("incorrect criticctl usage did not fail")
+
+expected = """\
+  id |    name    |              email             |            fullname            | status
+-----+------------+--------------------------------+--------------------------------+--------
+   1 |      admin |              admin@example.org | Testing Administrator          | current
+   2 |      alice |              alice@example.org | Alice von Testing              | current
+   3 |        bob |                bob@example.org | Bob von Testing                | current
+   4 |       dave |               dave@example.org | Dave von Testing               | current
+   5 |       erin |               erin@example.org | Erin von Testing               | current
+   6 |      extra |              extra@example.org | Extra von Testing              | retired
+
+"""
+
+# Scenario: Default / human readable format.
+try:
+    output = instance.execute(["sudo", "criticctl", "listusers"])
+except testing.virtualbox.GuestCommandError as error:
+    logger.error("correct criticctl usage failed:\n%s"
+                 % error.output)
+else:
+    testing.expect.check(expected, output)
+
+try:
+    output = instance.execute(["sudo", "criticctl", "listusers",
+                               "-f", "table"])
+except testing.virtualbox.GuestCommandError as error:
+    logger.error("correct criticctl usage failed:\n%s"
+                 % error.output)
+else:
+    testing.expect.check(expected, output)
+
+try:
+    output = instance.execute(["sudo", "criticctl", "listusers",
+                               "--format", "table"])
+except testing.virtualbox.GuestCommandError as error:
+    logger.error("correct criticctl usage failed:\n%s"
+                 % error.output)
+else:
+    testing.expect.check(expected, output)
+
+expected = """\
+# id, name, email, fullname, status
+[
+ (1, 'admin', 'admin@example.org', 'Testing Administrator', 'current'),
+ (2, 'alice', 'alice@example.org', 'Alice von Testing', 'current'),
+ (3, 'bob', 'bob@example.org', 'Bob von Testing', 'current'),
+ (4, 'dave', 'dave@example.org', 'Dave von Testing', 'current'),
+ (5, 'erin', 'erin@example.org', 'Erin von Testing', 'current'),
+ (6, 'extra', 'extra@example.org', 'Extra von Testing', 'retired'),
+]
+"""
+
+# Scenario: Tuples format.
+try:
+    output = instance.execute(["sudo", "criticctl", "listusers",
+                               "-f", "tuples"])
+except testing.virtualbox.GuestCommandError as error:
+    logger.error("correct criticctl usage failed:\n%s"
+                 % error.output)
+else:
+    testing.expect.check(expected, output)
+
+try:
+    output = instance.execute(["sudo", "criticctl", "listusers",
+                               "--format", "tuples"])
+except testing.virtualbox.GuestCommandError as error:
+    logger.error("correct criticctl usage failed:\n%s"
+                 % error.output)
+else:
+    testing.expect.check(expected, output)
+
+expected = """\
+[
+ {'id': 1, 'name': 'admin', 'email': 'admin@example.org', 'fullname': 'Testing Administrator', 'status': 'current'},
+ {'id': 2, 'name': 'alice', 'email': 'alice@example.org', 'fullname': 'Alice von Testing', 'status': 'current'},
+ {'id': 3, 'name': 'bob', 'email': 'bob@example.org', 'fullname': 'Bob von Testing', 'status': 'current'},
+ {'id': 4, 'name': 'dave', 'email': 'dave@example.org', 'fullname': 'Dave von Testing', 'status': 'current'},
+ {'id': 5, 'name': 'erin', 'email': 'erin@example.org', 'fullname': 'Erin von Testing', 'status': 'current'},
+ {'id': 6, 'name': 'extra', 'email': 'extra@example.org', 'fullname': 'Extra von Testing', 'status': 'retired'},
+]
+"""
+
+# Scenario: Dicts format.
+try:
+    output = instance.execute(["sudo", "criticctl", "listusers",
+                               "-f", "dicts"])
+except testing.virtualbox.GuestCommandError as error:
+    logger.error("correct criticctl usage failed:\n%s"
+                 % error.output)
+else:
+    testing.expect.check(expected, output)
+
+try:
+    output = instance.execute(["sudo", "criticctl", "listusers",
+                               "--format", "dicts"])
+except testing.virtualbox.GuestCommandError as error:
+    logger.error("correct criticctl usage failed:\n%s"
+                 % error.output)
+else:
+    testing.expect.check(expected, output)
