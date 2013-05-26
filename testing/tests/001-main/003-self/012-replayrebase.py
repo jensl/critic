4956df2ef824c2c92ecd5986fde5a221b5f198cd
Jens Lindstrom <jl@opera.com>
2013-05-26 12:44:50 +0200

    Add simple test performing a non-fast-forward review rebase
    Primarily testing that the "git cherry-pick" command used when replaying
    a rebase doesn't depend on the Critic system user's ~/.gitconfig setting
    user.name and user.email (which it doesn't in a default installation.)

---
+++ testing/tests/001-main/003-self/012-replayrebase.py
@@ -0,0 +1,71 @@
+import re
+
+def to(name):
+    return testing.mailbox.to_recipient("%s@example.org" % name)
+
+def about(subject):
+    return testing.mailbox.with_subject(subject)
+
+# The commit we'll be "reviewing."
+COMMIT_SHA1 = "aca57d0899e5193232dbbea726d94a838a4274ed"
+# The original parent of that commit.
+PARENT_SHA1 = "ca89553db7a2ba22fef70535a65beedf33c97216"
+# An ancestor of the original parent, onto which we'll be rebasing the reviewed
+# commit.
+TARGET_SHA1 = "132dbfb7c2ac0f4333fb483a70f1e8cce0333d11"
+
+# The subject of the reviewed commit.
+SUMMARY = "Use temporary clones for relaying instead of temporary remotes"
+
+with frontend.signin("alice"):
+    result = frontend.operation(
+        "savesettings",
+        data="review.createViaPush=1\n",
+        expect=None)
+
+    testing.expect.check("ok", result)
+
+    with repository.workcopy() as work:
+        work.run(["remote", "add", "critic",
+                  "alice@%s:/var/git/critic.git" % instance.hostname])
+        work.run(["checkout", "-b", "r/012-checkrebase", PARENT_SHA1])
+        work.run(["cherry-pick", COMMIT_SHA1],
+                 GIT_COMMITTER_NAME="Alice von Testing",
+                 GIT_COMMITTER_EMAIL="alice@example.org")
+
+        output = work.run(["push", "critic", "HEAD"])
+
+        for line in output.splitlines():
+            if not line.startswith("remote:"):
+                continue
+            line = line[len("remote:"):].split("\x1b", 1)[0].strip()
+            if not line.startswith("Submitted review:"):
+                continue
+            url = line[len("Submitted review:"):].strip()
+            review_id = int(re.search(r"/r/(\d+)$", url).group(1))
+            break
+        else:
+            testing.expect.check("<review URL in git hook output>",
+                                 "<expected content not found>")
+
+        to_alice = mailbox.pop(accept=[to("alice"),
+                                       about("New Review: %s" % SUMMARY)],
+                               timeout=30)
+        if not to_alice:
+            testing.expect.check("<mail to alice>",
+                                 "<expected mail not received>")
+
+        frontend.operation(
+            "preparerebase",
+            data={ "review_id": review_id,
+                   "new_upstream": TARGET_SHA1 })
+
+        work.run(["rebase", "--onto", TARGET_SHA1, PARENT_SHA1])
+        work.run(["push", "--force", "critic", "HEAD"])
+
+        to_alice = mailbox.pop(accept=[to("alice"),
+                                       about("Updated Review: %s" % SUMMARY)],
+                               timeout=30)
+        if not to_alice:
+            testing.expect.check("<mail to alice>",
+                                 "<expected mail not received>")
