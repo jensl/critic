7dbd6fb1dcd9638fa90ba62f6e5ff5f63374b296
Jens Lindstrom <jl@opera.com>
2013-07-08 13:28:53 +0200

    Extend 012-replayrebase.py to test a bit more
    Make it add some (quite a lot of, actually) files as part of the
    rebase, to trigger a non-empty "rebase diff" and to create a new
    pack file in the repository when pushing (which in turn causes the
    existence of an inaccessible pack-*.keep file when the rebase
    replay mechanism clones the repository, and thus tests our handling
    of that.)

--- testing/tests/001-main/003-self/012-replayrebase.py
+++ testing/tests/001-main/003-self/012-replayrebase.py
@@ -1,4 +1,6 @@
+import os
 import re
+import shutil
 
 def to(name):
     return testing.mailbox.to_recipient("%s@example.org" % name)
@@ -21,7 +23,9 @@ with frontend.signin("alice"):
     frontend.operation(
         "savesettings",
         data={ "settings": [{ "item": "review.createViaPush",
-                              "value": True }] })
+                              "value": True },
+                            { "item": "email.subjectLine.updatedReview.reviewRebased",
+                              "value": "Rebased Review: %(summary)s" }] })
 
     with repository.workcopy() as work:
         work.run(["remote", "add", "critic",
@@ -60,10 +64,38 @@ with frontend.signin("alice"):
                    "new_upstream": TARGET_SHA1 })
 
         work.run(["rebase", "--onto", TARGET_SHA1, PARENT_SHA1])
+
+        # Create some new files as part of the rebase.  This serves two
+        # purposes:
+        #
+        # 1) Generate some "changes introduced by rebase" for the rebase replay
+        #    mechanism to deal with, and
+        #
+        # 2) make sure the push creates a new pack file (a certain amount of new
+        #    objects are required to cause this) so that "git receive-pack"
+        #    creates a pack-*.keep file, which creates trouble for "git clone".
+        source_path = os.path.join(work.path, "testing")
+        for index in range(10):
+            destination_path = os.path.join(work.path, "testing%d" % index)
+            shutil.copytree(source_path, destination_path)
+            for path, _, filenames in os.walk(destination_path):
+                for filename in filenames:
+                    with open(os.path.join(path, filename), "a") as copied:
+                        copied.write("%d\n" % index)
+            work.run(["add", "testing%d" % index])
+        work.run(["commit", "--amend", "--reuse-message=HEAD"])
+
         work.run(["push", "--force", "critic", "HEAD"])
 
         to_alice = mailbox.pop(accept=[to("alice"),
                                        about("Updated Review: %s" % SUMMARY)],
+                               timeout=30)
+        if not to_alice:
+            testing.expect.check("<mail to alice>",
+                                 "<expected mail not received>")
+
+        to_alice = mailbox.pop(accept=[to("alice"),
+                                       about("Rebased Review: %s" % SUMMARY)],
                                timeout=30)
         if not to_alice:
             testing.expect.check("<mail to alice>",
