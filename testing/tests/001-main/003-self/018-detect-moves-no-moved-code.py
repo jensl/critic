e81ac918af027797606f2c36ca169f8d5b3f54c3
Martin Olsson <martin@minimum.se>
2013-09-04 21:12:07 +0200

    Add TC for "Properly show error when detecting moved code and there is none"

---
+++ testing/tests/001-main/003-self/018-detect-moves-no-moved-code.py
@@ -0,0 +1,13 @@
+# Scenario: Bob is viewing a commit that doesn't contain any chunks that Critic
+# detects as "moved code". Bob is not sure though, so he hits 'm', selects the
+# appropriate filenames and clicks SEARCH. Critic should not crash.
+
+COMMIT_WITH_NO_MOVES = 'cc1c1a25'
+
+with frontend.signin("bob"):
+    document = frontend.page(
+        "critic/%s" % COMMIT_WITH_NO_MOVES,
+        params={ "moves": "yes" },
+        expect={ "message": testing.expect.message_title(u"No moved code found!") })
+
+    mailbox.check_empty()
