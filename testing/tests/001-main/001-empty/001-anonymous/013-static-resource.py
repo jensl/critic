ba26df66333fd0894bb602175286e2dc3cfc74ac
Jens Lindstrom <jl@opera.com>
2013-09-12 11:45:49 +0200

    Drop "Indexes" and "MultiViews" options for /static-resource/
    There's not much point in producing a directory listing of the
    $INSTALL_DIR/resources/ directory, and we don't do content
    negotiation, so enabling the "MultiViews" option doesn't make
    much sense.
    
    We do need the FollowSymLinks option though, since the jQuery
    resources (such as jquery.js) are symlinks.
    
    Also add test that checks that directory listing is disabled,
    and that loading static resources works in general.

---
+++ testing/tests/001-main/001-empty/001-anonymous/013-static-resource.py
@@ -0,0 +1,35 @@
+def check_user_constructor(document):
+    expected = "<User constructor found>"
+    if "\nfunction User(" in document:
+        actual = expected
+    else:
+        actual = "<no User constructor found>"
+    testing.expect.check(expected, actual)
+
+def check_jquery_foundation(document):
+    expected = "<jQuery header found>"
+    if "jQuery Foundation, Inc" in document:
+        actual = expected
+    else:
+        actual = "<no jQuery header found>"
+    testing.expect.check(expected, actual)
+
+# Test a basic regular file.
+frontend.page(
+    "static-resource/basic.js",
+    expect={ "user_constructor": check_user_constructor })
+
+# Test jquery.js, which is a symlink to the current version.
+frontend.page(
+    "static-resource/jquery.js",
+    expect={ "jquery_foundation": check_jquery_foundation })
+
+# Test a non-existing file.
+frontend.page(
+    "static-resource/does-not-exist.js",
+    expected_http_status=404)
+
+# Test that directory listing is not enabled.
+frontend.page(
+    "static-resource/",
+    expected_http_status=403)
