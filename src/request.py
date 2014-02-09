jensl/critic

commit 8e83ce2d43b6e1f940acb6aafec8de9756176cb0

Add framework for authenticating users via external systems
Also add /createuser page, to which login attempts are redirected if no
Critic user is associated with the external account.

Also add an email verification mechanism (verifying that a user really
controls the email address registered) and more advanced primary email
handling, supporting multiple registered email addresses (one of which
is "selected" and actually user.)

src/request.py
@@ -22,6 +22,11 @@ import wsgiref.util
 import base
 import configuration
 
+# Paths to which access should be allowed without authentication even if
+# anonymous users are not allowed in general.
+INSECURE_PATHS = set(["login", "validatelogin",
+                      "createuser", "registeruser"])
+
 def decodeURIComponent(text):
     """\
     Replace %HH escape sequences and return the resulting string.
@@ -48,15 +53,24 @@ class MovedTemporarily(Exception):
 class NeedLogin(MovedTemporarily):
     def __init__(self, source, optional=False):
         if isinstance(source, Request):
-            target = "/" + source.path
-            if source.query:
-                target += "?" + source.query
+            target = source.getTargetURL()
         else:
             target = str(source)
         location = "/login?target=%s" % urllib.quote(target)
         if optional:
             location += "&optional=yes"
         super(NeedLogin, self).__init__(location, no_cache=True)
+
+class DoExternalAuthentication(Exception):
+    def __init__(self, provider_name, target_url=None):
+        self.provider_name = provider_name
+        self.target_url = target_url
+    def execute(self, db, req):
+        import auth
+        provider = auth.PROVIDERS[self.provider_name]
+        if not provider.start(db, req, self.target_url):
+            req.setStatus(403)
+            req.start()
 
 class DisplayMessage(base.Error):
     """\
@@ -181,7 +195,7 @@ class Request:
                 self.user = self.__environ["REMOTE_USER"]
             except KeyError:
                 raise MissingWSGIRemoteUser
-        elif configuration.base.AUTHENTICATION_MODE == "critic":
+        else:
             session_type = configuration.base.SESSION_TYPE
 
             authorization_header = self.getRequestHeader("Authorization")
@@ -202,15 +216,17 @@ class Request:
                                    WHERE key=%s""",
                                (sid,))
 
-                try:
-                    user, session_age = cursor.fetchone()
-                except:
+                row = cursor.fetchone()
+
+                if not row:
                     if self.path != "validatelogin":
                         cookie = "sid=invalid; Expires=Thursday 01-Jan-1970 00:00:00 GMT"
                         self.addResponseHeader("Set-Cookie", cookie)
-                    if self.path in ("login", "validatelogin"):
+                    if self.path in INSECURE_PATHS:
                         return
                     raise NeedLogin(self, optional=True)
+
+                user, session_age = row
 
                 if configuration.base.SESSION_MAX_AGE == 0 \
                         or session_age < configuration.base.SESSION_MAX_AGE:
@@ -243,6 +259,12 @@ class Request:
                         self.user = username
                         return
                     except auth.CheckFailed: pass
+
+    def getTargetURL(self):
+        target = "/" + self.path
+        if self.query:
+            target += "?" + self.query
+        return target
 
     def getRequestURI(self):
         return wsgiref.util.request_uri(self.__environ)
@@ -394,6 +416,19 @@ class Request:
         assert not self.__started, "Response already started!"
         assert name.lower() != "content-type", "Use Request.setContentType() instead!"
         self.__response_headers.append((name, value))
+
+    def setCookie(self, name, value, secure=False):
+        if secure and configuration.base.ACCESS_SCHEME != "http":
+            modifier = "Secure"
+        else:
+            modifier = "HttpOnly"
+        self.addResponseHeader("Set-Cookie",
+                               "%s=%s; Path=/; %s" % (name, value, modifier))
+
+    def deleteCookie(self, name):
+        self.addResponseHeader(
+            "Set-Cookie",
+            "%s=invalid; Path=/; Expires=Thursday 01-Jan-1970 00:00:00 GMT" % name)
 
     def start(self):
         """\

installation/config.py
@@ -24,11 +24,15 @@ import argparse
 
 import installation
 
+SENSITIVE_FILES = ("database.py", "smtp.py", "auth.py")
+
 auth_mode = "host"
 session_type = None
 allow_anonymous_user = None
 access_scheme = None
 repository_url_types = ["http"]
+allow_user_registration = None
+verify_email_addresses = True
 
 password_hash_schemes = ["pbkdf2_sha256", "bcrypt"]
 default_password_hash_scheme = "pbkdf2_sha256"
@@ -38,6 +42,68 @@ minimum_rounds = {}
 is_development = False
 is_testing = False
 coverage_dir = None
+
+class Provider(object):
+    def __init__(self, name):
+        self.name = name
+        self.enabled = False
+        self.allow_user_registration = False
+        self.verify_email_addresses = False
+        self.client_id = None
+        self.client_secret = None
+        self.redirect_uri = None
+        self.bypass_createuser = False
+
+    def load(self, settings):
+        if self.name not in settings:
+            return
+        settings = settings[self.name]
+        self.enabled = settings.get("enabled", self.enabled)
+        self.allow_user_registration = settings.get("allow_user_registration",
+                                                    self.allow_user_registration)
+        self.verify_email_addresses = settings.get("verify_email_addresses",
+                                                   self.verify_email_addresses)
+        self.client_id = settings.get("client_id", self.client_id)
+        self.client_secret = settings.get("client_secret", self.client_secret)
+        self.redirect_uri = settings.get("redirect_uri", self.redirect_uri)
+        self.bypass_createuser = settings.get("bypass_createuser",
+                                              self.bypass_createuser)
+
+    def readargs(self, arguments):
+        def getarg(name, default):
+            value = getattr(arguments, name, None)
+            if value is None:
+                return default
+            return value
+
+        self.enabled = getarg(
+            "provider_%s_enabled" % self.name, self.enabled)
+        self.allow_user_registration = getarg(
+            "provider_%s_user_registration" % self.name,
+            self.allow_user_registration)
+        self.verify_email_addresses = getarg(
+            "provider_%s_verify_email_addresses" % self.name,
+            self.verify_email_addresses)
+        self.client_id = getarg(
+            "provider_%s_client_id" % self.name, self.client_id)
+        self.client_secret = getarg(
+            "provider_%s_client_secret" % self.name, self.client_secret)
+        self.redirect_uri = getarg(
+            "provider_%s_redirect_uri" % self.name, self.redirect_uri)
+
+    def store(self, data):
+        base = "installation.config.provider_%s." % self.name
+
+        data[base + "enabled"] = self.enabled
+        data[base + "allow_user_registration"] = self.allow_user_registration
+        data[base + "verify_email_addresses"] = self.verify_email_addresses
+        data[base + "client_id"] = self.client_id
+        data[base + "client_secret"] = self.client_secret
+        data[base + "redirect_uri"] = self.redirect_uri
+        data[base + "bypass_createuser"] = self.bypass_createuser
+
+providers = []
+default_provider_names = ["github", "google"]
 
 def calibrate_minimum_rounds():
     import time
@@ -98,12 +164,53 @@ def add_arguments(mode, parser):
         "--no-allow-anonymous-user", dest="anonymous", action="store_const",
         const=False, help=H("do not allow unauthenticated access"))
     parser.add_argument(
+        "--allow-user-registration", dest="user_registration",
+        action="store_const", const=True,
+        help=H("allow unattended user registration"))
+    parser.add_argument(
+        "--no-allow-user-registration", dest="user_registration",
+        action="store_const", const=False,
+        help=H("do not allow unattended user registration"))
+    parser.add_argument(
         "--access-scheme", choices=["http", "https", "both"],
         help=H("scheme used to access Critic"))
     parser.add_argument(
         "--repository-url-types", default="http",
         help=H("comma-separated list of supported repository URL types "
                "(valid types: git, http, ssh and host)"))
+
+    for provider_name in default_provider_names:
+        if mode == "install":
+            group = parser.add_argument_group(
+                "'%s' authentication provider" % provider_name)
+        else:
+            group = parser
+
+        group.add_argument(
+            "--provider-%s-enabled" % provider_name, action="store_const",
+            const=True, help=H("enable authentication provider"))
+        group.add_argument(
+            "--provider-%s-disabled" % provider_name, action="store_const",
+            const=False, dest="provider_%s_enabled" % provider_name,
+            help=H("disable authentication provider"))
+        group.add_argument(
+            "--provider-%s-user-registration" % provider_name,
+            action="store_const", const=True,
+            help=H("enable new user registration"))
+        group.add_argument(
+            "--provider-%s-no-user-registration" % provider_name,
+            action="store_const", const=False,
+            dest="provider_%s_user_registration" % provider_name,
+            help=H("disable new user registration"))
+        group.add_argument(
+            "--provider-%s-client-id" % provider_name, action="store",
+            help=H("OAuth2 client id"))
+        group.add_argument(
+            "--provider-%s-client-secret" % provider_name, action="store",
+            help=H("OAuth2 client secret"))
+        group.add_argument(
+            "--provider-%s-redirect-uri" % provider_name, action="store",
+            help=H("OAuth2 authentication callback URI"))
 
     parser.add_argument(
         "--minimum-password-hash-time",
@@ -122,7 +229,8 @@ default_encodings = ["utf-8", "latin-1"]
 
 def prepare(mode, arguments, data):
     global auth_mode, session_type, allow_anonymous_user, access_scheme
-    global repository_url_types, default_encodings
+    global repository_url_types, default_encodings, allow_user_registration
+    global verify_email_addresses
     global password_hash_schemes, default_password_hash_scheme
     global minimum_password_hash_time, minimum_rounds
     global is_development, is_testing, coverage_dir
@@ -231,6 +339,12 @@ the Web front-end.  This can be handled in two different ways:
         try: coverage_dir = configuration.debug.COVERAGE_DIR
         except AttributeError: pass
 
+        try: allow_user_registration = configuration.base.ALLOW_USER_REGISTRATION
+        except AttributeError: pass
+
+        try: verify_email_addresses = configuration.base.VERIFY_EMAIL_ADDRESSES
+        except AttributeError: pass
+
     if auth_mode == "critic":
         if session_type is None:
             def check_session_type(value):
@@ -287,6 +401,27 @@ in the system without signin in.
                 allow_anonymous_user = installation.input.yes_or_no(
                     "Do you want to allow anonymous access?", default=True)
 
+        if allow_user_registration is None:
+            if session_type == "httpauth":
+                allow_user_registration = False
+            elif arguments.user_registration is not None:
+                allow_user_registration = arguments.user_registration
+            else:
+                if not header_printed:
+                    header_printed = True
+                    print """
+Critic Installation: Authentication
+==================================="""
+
+                print """
+With cookie based authentication, Critic can support unattended user
+registration.  With this enabled, the "Sign in" page has a link to a
+page where a new user can register a Critic user without needing to
+contact the system administrator(s).
+"""
+
+                allow_user_registration = installation.input.yes_or_no(
+                    "Do you want to allow user registration?", default=False)
     else:
         session_type = "cookie"
 
@@ -332,12 +467,37 @@ web server to redirect all HTTP accesses to HTTPS.
                 "How will Critic be accessed?", default="http",
                 check=check_access_scheme)
 
+    if mode == "upgrade" \
+           and hasattr(configuration, "auth") \
+           and hasattr(configuration.auth, "PROVIDERS"):
+        for provider_name in configuration.auth.PROVIDERS:
+            provider = Provider(provider_name)
+            provider.load(configuration.auth.PROVIDERS)
+            providers.append(provider)
+    else:
+        providers.extend(Provider(provider_name)
+                         for provider_name in default_provider_names)
+
+    if access_scheme == "http":
+        base_url = "http"
+    else:
+        base_url = "https"
+
+    base_url += "://%s/oauth/" % installation.system.hostname
+
+    for provider in providers:
+        provider.readargs(arguments)
+        if provider.redirect_uri is None:
+            provider.redirect_uri = base_url + provider.name
+
     data["installation.config.auth_mode"] = auth_mode
     data["installation.config.session_type"] = session_type
     data["installation.config.allow_anonymous_user"] = allow_anonymous_user
     data["installation.config.access_scheme"] = access_scheme
     data["installation.config.repository_url_types"] = repository_url_types
     data["installation.config.default_encodings"] = default_encodings
+    data["installation.config.allow_user_registration"] = allow_user_registration
+    data["installation.config.verify_email_addresses"] = verify_email_addresses
 
     calibrate_minimum_rounds()
 
@@ -349,6 +509,9 @@ web server to redirect all HTTP accesses to HTTPS.
     data["installation.config.is_development"] = is_development
     data["installation.config.is_testing"] = is_testing
     data["installation.config.coverage_dir"] = coverage_dir
+
+    for provider in providers:
+        provider.store(data)
 
     return True
 
@@ -390,7 +553,7 @@ def install(data):
             with open(target_path, "w") as target:
                 created_file.append(target_path)
 
-                if entry in ("database.py", "smtp.py"):
+                if entry in SENSITIVE_FILES:
                     # May contain secrets (passwords.)
                     mode = 0600
                 else:
@@ -485,7 +648,7 @@ configuration options to the existing version.
 
                 with open(target_path, "w") as target:
                     created_file.append(target_path)
-                    if entry in ("database.py", "smtp.py"):
+                    if entry in SENSITIVE_FILES:
                         # May contain secrets (passwords.)
                         mode = 0600
                     else:

installation/data/dbschema.sql
@@ -38,9 +38,19 @@ CREATE TABLE users
     name VARCHAR(64) NOT NULL UNIQUE,
     fullname VARCHAR(256),
     password VARCHAR(256),
-    email VARCHAR(256),
+    email INTEGER, -- Foreign key constraint "REFERENCES users" set up later.
     status userstatus NOT NULL DEFAULT 'unknown' );
-CREATE INDEX users_email ON users (email);
+
+CREATE TABLE useremails
+  ( id SERIAL PRIMARY KEY,
+    uid INTEGER NOT NULL REFERENCES users ON DELETE CASCADE,
+    email VARCHAR(256) NOT NULL,
+    verified BOOLEAN,
+    verification_token VARCHAR(256),
+
+    UNIQUE (uid, email) );
+
+ALTER TABLE users ADD CONSTRAINT users_email_fkey FOREIGN KEY (email) REFERENCES useremails;
 
 CREATE TABLE usersessions
   ( key CHAR(28) PRIMARY KEY,
@@ -53,7 +63,6 @@ CREATE TABLE gitusers
     fullname VARCHAR(256) NOT NULL,
 
     UNIQUE (email, fullname) );
-CREATE INDEX gitusers_email ON users (email);
 
 CREATE TABLE usergitemails
   ( email VARCHAR(256) PRIMARY KEY,
@@ -574,3 +583,18 @@ CREATE UNIQUE INDEX userpreferences_item_uid_filter
               WHERE uid IS NOT NULL
                 AND repository IS NULL
                 AND filter IS NOT NULL;
+
+CREATE TABLE externalusers
+  ( id SERIAL PRIMARY KEY,
+    uid INTEGER REFERENCES users,
+    provider VARCHAR(16) NOT NULL,
+    account VARCHAR(256) NOT NULL,
+    email VARCHAR(256),
+    token VARCHAR(256),
+
+    UNIQUE (provider, account) );
+
+CREATE TABLE oauthstates
+  ( state VARCHAR(64) PRIMARY KEY,
+    url TEXT,
+    time TIMESTAMP NOT NULL DEFAULT NOW() );

installation/migrations/dbschema.createtable.useremails.py
@@ -1,0 +1,76 @@
+# -*- mode: python; encoding: utf-8 -*-
+#
+# Copyright 2014 the Critic contributors, Opera Software ASA
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License.  You may obtain a copy of
+# the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+# License for the specific language governing permissions and limitations under
+# the License.
+
+import sys
+import psycopg2
+import json
+import argparse
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--uid", type=int)
+parser.add_argument("--gid", type=int)
+
+arguments = parser.parse_args()
+
+os.setgid(arguments.gid)
+os.setuid(arguments.uid)
+
+data = json.load(sys.stdin)
+
+import configuration
+
+db = psycopg2.connect(**configuration.database.PARAMETERS)
+cursor = db.cursor()
+
+try:
+    # Make sure the table doesn't already exist.
+    cursor.execute("SELECT 1 FROM useremails")
+
+    # Above statement should have thrown a psycopg2.ProgrammingError, but it
+    # didn't, so just exit.
+    sys.exit(0)
+except psycopg2.ProgrammingError: db.rollback()
+except: raise
+
+# Create the table.
+cursor.execute("""CREATE TABLE useremails
+                    ( id SERIAL PRIMARY KEY,
+                      uid INTEGER NOT NULL REFERENCES users ON DELETE CASCADE,
+                      email VARCHAR(256) NOT NULL,
+                      verified BOOLEAN,
+                      verification_token VARCHAR(256),
+
+                      UNIQUE (uid, email) )""")
+
+# Create records for all current email addresses in the system.  Set verified to
+# NULL which means the addresses can be used, but that they haven't gone through
+# the verification process.
+cursor.execute("""INSERT INTO useremails (uid, email)
+                       SELECT id, email
+                         FROM users
+                        WHERE email IS NOT NULL""")
+
+# Drop the old 'users.email' column.
+cursor.execute("ALTER TABLE users DROP email")
+
+# And create a new one based on the information we copied over to the new table.
+cursor.execute("ALTER TABLE users ADD email INTEGER REFERENCES useremails")
+cursor.execute("SELECT id, uid FROM useremails")
+cursor.executemany("UPDATE users SET email=%s WHERE id=%s", cursor.fetchall())
+
+db.commit()
+db.close()

installation/migrations/dbschema.external-authentication.py
@@ -1,0 +1,74 @@
+# -*- mode: python; encoding: utf-8 -*-
+#
+# Copyright 2014 the Critic contributors, Opera Software ASA
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License.  You may obtain a copy of
+# the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+# License for the specific language governing permissions and limitations under
+# the License.
+
+import psycopg2
+import argparse
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--uid", type=int)
+parser.add_argument("--gid", type=int)
+
+arguments = parser.parse_args()
+
+os.setgid(arguments.gid)
+os.setuid(arguments.uid)
+
+import configuration
+
+db = psycopg2.connect(**configuration.database.PARAMETERS)
+cursor = db.cursor()
+
+def create(table_name, statement):
+    try:
+        # Make sure the table doesn't already exist.
+        cursor.execute("SELECT 1 FROM %s" % table_name)
+
+        # Above statement would have thrown a psycopg2.ProgrammingError if the
+        # table didn't exist, but it didn't, so assume the table doesn't need to
+        # be added.
+        return
+    except psycopg2.ProgrammingError:
+        db.rollback()
+
+    cursor.execute(statement)
+    db.commit()
+
+create("externalusers", """
+
+CREATE TABLE externalusers
+  ( id SERIAL PRIMARY KEY,
+    uid INTEGER REFERENCES users,
+    provider VARCHAR(16) NOT NULL,
+    account VARCHAR(256) NOT NULL,
+    email VARCHAR(256),
+    token VARCHAR(256),
+
+    UNIQUE (provider, account) );
+
+""")
+
+create("oauthstates", """
+
+CREATE TABLE oauthstates
+  ( state VARCHAR(64) PRIMARY KEY,
+    url TEXT,
+    time TIMESTAMP NOT NULL DEFAULT NOW() );
+
+""")
+
+db.commit()
+db.close()

installation/prereqs.py
@@ -40,6 +40,7 @@ a2dissite = None
 psycopg2_available = False
 pygments_available = False
 passlib_available = False
+requests_available = False
 
 aptget = None
 aptget_approved = False
@@ -56,7 +57,8 @@ def blankline():
 def check(mode, arguments):
     global git, tar, psql, passlib_available, aptget, apache2ctl, a2enmod, a2ensite, a2dissite
 
-    print """
+    if mode == "install":
+        print """
 Critic Installation: Prerequisites
 ==================================
 """
@@ -345,7 +347,34 @@ so you might just need to restart this script."""
         if install_passlib and not passlib_available:
             success = False
 
-    if all_ok: print "All prerequisites available."
+    def check_requests():
+        global requests_available
+        try:
+            import requests
+            requests_available = True
+        except ImportError: pass
+
+    check_requests()
+    if not requests_available:
+        if aptget_approved and install("python-requests"):
+            check_requests()
+        if not requests_available:
+            blankline()
+            all_ok = False
+            print """\
+Failed to import the 'requests' module, which is used to perform URL requests.
+In Debian/Ubuntu, the module is provided by the 'python-requests' package.  The
+source code can be downloaded here:
+
+  https://github.com/kennethreitz/requests
+"""
+        if not aptget_approved and install("python-requests"):
+            check_requests()
+        if not requests_available:
+            success = False
+
+    if mode == "install" and all_ok:
+        print "All prerequisites available."
 
     return success
 

installation/templates/configuration/auth.py
@@ -28,3 +28,55 @@ MINIMUM_PASSWORD_HASH_TIME = %(installation.config.minimum_password_hash_time)r
 
 # Calibrated minimum rounds per password hash scheme.
 MINIMUM_ROUNDS = %(installation.config.minimum_rounds)r
+
+# External authentication providers.
+PROVIDERS = {
+
+    # GitHub OAuth-based authentication.
+    "github": {
+        "enabled": %(installation.config.provider_github.enabled)r,
+
+        # Allow authenticated user to create a Critic user.
+        "allow_user_registration": %(installation.config.provider_github.allow_user_registration)r,
+        # Verify user email addresses provided by GitHub.
+        "verify_email_addresses": %(installation.config.provider_github.verify_email_addresses)r,
+
+        # Client ID and secret.  These are generated by registering an
+        # application at https://github.com/settings/applications/new.
+        "client_id": %(installation.config.provider_github.client_id)r,
+        "client_secret": %(installation.config.provider_github.client_secret)r,
+
+        # Bypass /createuser on first sign in, creating a user automatically.
+        "bypass_createuser": %(installation.config.provider_github.bypass_createuser)r,
+
+        # Authentication callback URI.  This same URI must be provided
+        # to GitHub when registering the application.  The path
+        # component must be "/oauth/github".
+        "redirect_uri": %(installation.config.provider_github.redirect_uri)r
+    },
+
+    # Google OAuth-based authentication.
+    "google": {
+        "enabled": %(installation.config.provider_google.enabled)r,
+
+        # Allow authenticated user to create a Critic user.
+        "allow_user_registration": %(installation.config.provider_google.allow_user_registration)r,
+        # Verify user email addresses provided by Google.
+        "verify_email_addresses": %(installation.config.provider_google.verify_email_addresses)r,
+
+        # Client ID and secret.  These are generated by creating a project at
+        # https://cloud.google.com/console/project, and then creating an OAuth2
+        # client id using the project administration UI.
+        "client_id": %(installation.config.provider_google.client_id)r,
+        "client_secret": %(installation.config.provider_google.client_secret)r,
+
+        # Bypass /createuser on first sign in, creating a user automatically.
+        "bypass_createuser": %(installation.config.provider_google.bypass_createuser)r,
+
+        # Authentication callback URI.  This same URI must be provided
+        # to Google when creating the OAuth2 client id.  The path
+        # component must be "/oauth/google".
+        "redirect_uri": %(installation.config.provider_google.redirect_uri)r
+    },
+
+}

commit 2b7794ce253032eccf18952063b7dfc83bc5e6d5

Set Max-Age directive on cookies again
Fixes regression introduced in 8e83ce2d "Add framework for authenticating
users via external systems", which moved the code that adds Set-Cookie
response headers from src/operation/__init__.py to src/request.py, and
reverted 4c9a1627 "Set Max-Age=<1 year> on all cookies" in the process.

src/request.py
@@ -422,8 +422,9 @@ class Request:
             modifier = "Secure"
         else:
             modifier = "HttpOnly"
-        self.addResponseHeader("Set-Cookie",
-                               "%s=%s; Path=/; %s" % (name, value, modifier))
+        self.addResponseHeader(
+            "Set-Cookie",
+            "%s=%s; Max-Age=31536000; Path=/; %s" % (name, value, modifier))
 
     def deleteCookie(self, name):
         self.addResponseHeader(