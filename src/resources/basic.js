jensl/critic

commit 5ec60e304dfcaf5e438a111bb85ce7aeef595796

Support linking to review searches
Add handling of URL parameters to /search, mapping "q<term>=<value>" to
"<term>:<value>" and "q=<value>" to "<value>".  When any such parameters
is supplied, /search renders with a single input field (containing the
resulting "quick search" query) and inlined search results.

Also add a link to such a URL in the "Search results" dialog opened for
quick searches (on any page) and regular searches on /search.

src/resources/basic.js
@@ -437,7 +437,7 @@ var previous_query = "";
 if (typeof localStorage != "undefined")
   previous_query = localStorage.getItem("previous_query");
 
-function quickSearch(external_query)
+function quickSearch(external_query, callback)
 {
   function finish(result)
   {
@@ -454,12 +454,14 @@ function quickSearch(external_query)
       return;
     }
 
-    var html = ("<div title='Search results'><table class=searchresults>" +
-                "<tr><th class=id>Review</th><th class=summary>Summary</th></tr>");
+    var html = ("<table class=searchresults>" +
+                "<tr><th class=id>Review</th><th class=summary>Summary</th>");
+
+    html += "</tr>";
 
     result.reviews.forEach(function (review, index)
       {
-        html += ("<tr critic-review-id=" + review.id + ">" +
+        html += ("<tr class=review critic-review-id=" + review.id + ">" +
                  "<td class=id>r/" + review.id + "</td>" +
                  "<td class=summary><a href=/r/" + review.id + ">" + htmlify(review.summary) + "</a></td>" +
                  "</tr>");
@@ -476,11 +478,24 @@ function quickSearch(external_query)
           target.parents("tr").find("a").get(0).click();
       });
 
-    content.dialog({ width: 800,
-                     buttons: { "Close": function () { content.dialog("close"); }}});
+    if (callback)
+      callback(content, result);
+    else
+    {
+      content.wrap("<div title='Search results'></div>");
+      content.find("tr").first().append(
+        "<td class=link><a>Link to this search</a></td>");
+      content.find("a").attr("href", "/search?" + result.query_string);
+      content.find("td.summary").attr("colspan", "2");
 
-    if (content.closest(".ui-dialog").height() > innerHeight)
-      content.dialog("option", "height", innerHeight - 10);
+      content = content.parent();
+      content.dialog(
+        { width: 800,
+          buttons: { "Close": function () { content.dialog("close"); }}});
+
+      if (content.closest(".ui-dialog").height() > innerHeight)
+        content.dialog("option", "height", innerHeight - 10);
+    }
   }
 
   function search(query)

src/operation/searchreview.py
@@ -15,6 +15,7 @@
 # the License.
 
 import re
+import urllib
 
 import dbutils
 import gitutils
@@ -173,11 +174,15 @@ class SearchReview(Operation):
 
     def process(req, db, user, query):
         terms = re.findall("""((?:"[^"]*"|'[^']*'|[^ "']+)+)""", query)
+        url_terms = []
         filters = []
 
         for term in terms:
             if re.match("[a-z\-]+:", term):
                 keyword, _, value = term.partition(":")
+
+                url_terms.append(("q" + keyword, value))
+
                 if keyword == "summary":
                     filter_classes = [SummaryFilter]
                 elif keyword == "description":
@@ -236,6 +241,8 @@ class SearchReview(Operation):
                         message=("The search term following %r must be a valid repository name."
                                  % (keyword + ":")))
             else:
+                url_terms.append(("q", term))
+
                 if re.match("([\"']).*\\1$", term):
                     term = term[1:-1]
 
@@ -279,4 +286,6 @@ class SearchReview(Operation):
         for search_filter in filters:
             reviews = filter(lambda review: search_filter.filter(db, review), reviews)
 
-        return OperationResult(reviews=map(Review.json, reviews))
+        return OperationResult(
+            reviews=map(Review.json, reviews),
+            query_string=urllib.urlencode(url_terms))

src/page/search.py
@@ -14,6 +14,8 @@
 # License for the specific language governing permissions and limitations under
 # the License.
 
+import urlparse
+
 import htmlutils
 import textutils
 import page.utils
@@ -97,22 +99,46 @@ def renderSearch(req, db, user):
     section = body.section("paleyellow section")
     section.h1("section-heading").text("Review Search")
 
+    url_terms = []
+
+    for name, value in urlparse.parse_qsl(req.query):
+        if name == "q":
+            url_terms.append(value)
+        elif name.startswith("q"):
+            url_terms.append("%s:%s" % (name[1:], value))
+
     wrap = section.div("flex")
     search = wrap.form("search", name="search")
 
-    row = search.div("flex")
-    renderFreetext(row)
-    renderState(row)
+    if url_terms:
+        row = search.div("flex")
+        query = row.fieldset("search-query")
+        query.label("input-label").text("Search query")
+        query.input(type="text", name="query", value=" ".join(url_terms))
 
-    renderUser(search)
+        result = section.div("search-result", style="display: none")
+        result.h2().text("Search result")
+        result.div("callout")
+    else:
+        row = search.div("flex")
+        renderFreetext(row)
+        renderState(row)
 
-    row = search.div("flex")
-    renderRepository(row)
-    renderInput(row, "Branch", "branch")
+        renderUser(search)
 
-    renderInput(search, "Path", "path")
+        row = search.div("flex")
+        renderRepository(row)
+        renderInput(row, "Branch", "branch")
 
-    search.button(type="submit").text("Search")
+        renderInput(search, "Path", "path")
+
+    buttons = search.div("search-buttons")
+
+    if url_terms:
+        buttons.button(type="submit").text("Search again")
+        buttons.a("button", href="/search").text("Show full search form")
+    else:
+        buttons.button(type="submit").text("Search")
 
     renderQuickSearch(wrap)
 

src/resources/basic.css
@@ -360,7 +360,12 @@ table.searchresults .summary {
     padding-left: 0.5em
 }
 
-table.searchresults td {
+table.searchresults td.link {
+    text-align: right;
+    vertical-align: top;
+}
+
+table.searchresults tr.review td {
     font-family: monospace;
     padding-top: 3px;
     padding-bottom: 3px;

src/resources/search.css
@@ -52,6 +52,10 @@
     width: 100%;
 }
 
+.search-query {
+    width: 100%;
+}
+
 .search-freetext,
 .search-repository {
     margin-right: 1.5rem;
@@ -72,4 +76,22 @@
     -ms-flex: 2;
     -webkit-flex: 2;
     flex: 2;
+}
+
+.search-buttons > * {
+    margin-right: 0.5rem
+}
+
+.search-result {
+    margin: 0 20%
+}
+
+.search-result h2 {
+    font-weight: normal;
+    font-size: 1.4rem;
+    margin: 1rem 0 0.5rem 0
+}
+
+.search-result tr.review td {
+    background-color: inherit !important
 }

src/resources/search.js
@@ -21,18 +21,31 @@
 $(function () {
   var searchForm = document.forms.search;
 
-  // Disable the freetext input if none of its checkboxes are checked
-  [ searchForm.freetextSummary, searchForm.freetextDescription ].forEach(
-    function (checkbox, idx, checkboxes) {
-      checkbox.addEventListener('click', function () {
-        var someChecked = checkboxes.some(
-          function (cbox) { return cbox.checked; }
-        );
-        if (searchForm.freetext.disabled === someChecked) {
-          searchForm.freetext.disabled = !someChecked;
-        }
+  function doSearch(query) {
+    function handleResult(table, result) {
+      $(".search-result")
+        .css({ display: "block" })
+        .children(".callout").empty().append(table);
+      history.replaceState(null, null, "/search?" + result.query_string);
+    }
+
+    quickSearch(query, searchForm.query ? handleResult : null);
+  }
+
+  if (!searchForm.query) {
+    // Disable the freetext input if none of its checkboxes are checked
+    [ searchForm.freetextSummary, searchForm.freetextDescription ].forEach(
+      function (checkbox, idx, checkboxes) {
+        checkbox.addEventListener('click', function () {
+          var someChecked = checkboxes.some(
+            function (cbox) { return cbox.checked; }
+          );
+          if (searchForm.freetext.disabled === someChecked) {
+            searchForm.freetext.disabled = !someChecked;
+          }
+        });
       });
-    });
+  }
 
   searchForm.addEventListener('submit', function (evt) {
     evt.preventDefault();
@@ -66,51 +79,59 @@ $(function () {
       return function (term) { return term ? keyword + ":'" + term + "'" : ""; };
     }
 
-    var terms = [];
+    var query;
 
-    var freetext = form.freetext.value.trim();
-    if (freetext) {
-      var textphrases = phrases(freetext);
-      if (form.freetextSummary.checked && form.freetextDescription.checked) {
-        terms.push.apply(terms, textphrases.map(with_keyword("text")));
-      } else if (form.freetextSummary.checked) {
-        terms.push.apply(terms, textphrases.map(with_keyword("summary")));
-      } else if (form.freetextDescription.checked) {
-        terms.push.apply(terms, textphrases.map(with_keyword("description")));
-      }
-    }
-
-    var users = tokens(form.user.value.trim()), usersKey;
-    if (form.userOwner.checked && form.userReviewer.checked) {
-      usersKey = "owner-or-reviewer";
-    } else if (form.userOwner.checked) {
-      usersKey = "owner";
-    } else if (form.userReviewer.checked) {
-      usersKey = "reviewer";
+    if (form.query) {
+      query = form.query.value.trim();
     } else {
-      usersKey = "user";
+      var terms = [];
+
+      var freetext = form.freetext.value.trim();
+      if (freetext) {
+        var textphrases = phrases(freetext);
+        if (form.freetextSummary.checked && form.freetextDescription.checked) {
+          terms.push.apply(terms, textphrases.map(with_keyword("text")));
+        } else if (form.freetextSummary.checked) {
+          terms.push.apply(terms, textphrases.map(with_keyword("summary")));
+        } else if (form.freetextDescription.checked) {
+          terms.push.apply(terms, textphrases.map(with_keyword("description")));
+        }
+      }
+
+      var users = tokens(form.user.value.trim()), usersKey;
+      if (form.userOwner.checked && form.userReviewer.checked) {
+        usersKey = "owner-or-reviewer";
+      } else if (form.userOwner.checked) {
+        usersKey = "owner";
+      } else if (form.userReviewer.checked) {
+        usersKey = "reviewer";
+      } else {
+        usersKey = "user";
+      }
+      terms.push.apply(terms, users.map(with_keyword(usersKey)));
+
+      var repository = form.repository.value;
+      if (repository) {
+        terms.push(with_keyword("repository")(repository));
+      }
+
+      var branch = form.branch.value.trim();
+      if (branch) {
+        terms.push(with_keyword("branch")(branch));
+      }
+
+      var paths = tokens(form.path.value.trim());
+      terms.push.apply(terms, paths.map(with_keyword("path")));
+
+      var state = form.state.value;
+      if (state) {
+        terms.push(with_keyword("state")(state));
+      }
+
+      query = terms.join(" ");
     }
-    terms.push.apply(terms, users.map(with_keyword(usersKey)));
 
-    var repository = form.repository.value;
-    if (repository) {
-      terms.push(with_keyword("repository")(repository));
-    }
-
-    var branch = form.branch.value.trim();
-    if (branch) {
-      terms.push(with_keyword("branch")(branch));
-    }
-
-    var paths = tokens(form.path.value.trim());
-    terms.push.apply(terms, paths.map(with_keyword("path")));
-
-    var state = form.state.value;
-    if (state) {
-      terms.push(with_keyword("state")(state));
-    }
-
-    quickSearch(terms.join(" "));
+    doSearch(query);
   });
 
   $(document.forms.search.user)
@@ -123,4 +144,8 @@ $(function () {
               allow_single_deselect: true,
               collapsed_width: "100%",
               expanded_width: "40em" });
+
+  if (searchForm.query && searchForm.query.value.trim()) {
+    doSearch(searchForm.query.value.trim());
+  }
 });

commit 7c856dc430144efb175fc290f030b1d2ebae0c5b

Actually link to reviews in search result page
Fixes regression introduced in 5ec60e30 "Support linking to review
searches", which accidentally sets the href attribute of all anchor
elements in the dialog to the URL of the search.

src/resources/basic.js
@@ -485,7 +485,7 @@ function quickSearch(external_query, callback)
       content.wrap("<div title='Search results'></div>");
       content.find("tr").first().append(
         "<td class=link><a>Link to this search</a></td>");
-      content.find("a").attr("href", "/search?" + result.query_string);
+      content.find("td.link a").attr("href", "/search?" + result.query_string);
       content.find("td.summary").attr("colspan", "2");
 
       content = content.parent();