jensl/critic

commit 5e61ca9e9fe90cae707e6cf758501eb3d202b18b

During installation, add administrator user using criticctl
This is quite a bit simpler.  And since we depended on the (installed)
'auth' module already, we might as well depend on more installed things.
We just need to make sure to install the criticctl executable a bit
earlier, which is uncomplicated since the installation of it doesn't
depend on anything but knowing installation paths.

installation/admin.py
@@ -14,9 +14,7 @@
 # License for the specific language governing permissions and limitations under
 # the License.
 
-import sys
 import subprocess
-import os
 
 import installation
 
@@ -70,40 +68,20 @@ This user does not need to match a system user on this machine.
 def install(data):
     global password
 
-    import psycopg2
+    try:
+        subprocess.check_output(
+            [installation.criticctl.criticctl_path, "adduser",
+             "--name", username,
+             "--email", email,
+             "--fullname", fullname,
+             "--password", password])
 
-    def adapt(value):
-        return psycopg2.extensions.adapt(value).getquoted()
-
-    if password is not None:
-        try:
-            import auth
-        except ImportError:
-            password = installation.process.check_input(
-                [sys.executable, "-c",
-                 "import sys, auth; sys.stdout.write(auth.hashPassword(sys.stdin.read()))"],
-                stdin=password, stdout=subprocess.PIPE,
-                env={ "PYTHONPATH": ":".join([os.path.join(installation.paths.etc_dir, "main"),
-                                              installation.paths.install_dir]) })
-        else:
-            password = auth.hashPassword(password)
-
-    installation.process.check_input(
-        ["su", "-s", "/bin/sh", "-c", "psql -q -v ON_ERROR_STOP=1 -f -", installation.system.username],
-        stdin=("""INSERT INTO users (name, email, password, fullname, status)
-                       VALUES (%s, %s, %s, %s, 'current');"""
-               % (adapt(username),
-                  adapt(email),
-                  adapt(password),
-                  adapt(fullname))))
-
-    for role in ["administrator", "repositories", "newswriter"]:
-        installation.process.check_input(
-            ["su", "-s", "/bin/sh", "-c", "psql -q -v ON_ERROR_STOP=1 -f -", installation.system.username],
-            stdin=("""INSERT INTO userroles (uid, role)
-                           SELECT id, %s
-                             FROM users
-                            WHERE name=%s;"""
-                   % (adapt(role), adapt(username))))
+        for role in ["administrator", "repositories", "newswriter"]:
+            subprocess.check_output(
+                [installation.criticctl.criticctl_path, "addrole",
+                 "--name", username,
+                 "--role", role])
+    except subprocess.CalledProcessError:
+        return False
 
     return True

installation/__init__.py
@@ -37,11 +37,11 @@ import database
 import smtp
 import config
 import apache
+import criticctl
 import admin
 import initd
 import prefs
 import git
-import criticctl
 import migrate
 
 modules = [prereqs,
@@ -51,10 +51,10 @@ modules = [prereqs,
            database,
            config,
            apache,
+           criticctl,
            admin,
            smtp,
            initd,
            git,
-           criticctl,
            migrate,
            prefs]

installation/criticctl.py
@@ -18,12 +18,15 @@ import installation
 import os
 import os.path
 
+criticctl_path = None
 created_file = []
 renamed = []
 
 def install(data):
+    global criticctl_path
+
     source_path = os.path.join(installation.root_dir, "installation", "templates", "criticctl")
-    target_path = os.path.join("/usr", "bin", "criticctl")
+    target_path = criticctl_path = os.path.join("/usr", "bin", "criticctl")
 
     with open(target_path, "w") as target:
         created_file.append(target_path)

commit 3a67e3038d4a927648678f3c5e1b27ac69292c00

Fix crash during installation in "--auth-mode host" mode
This crash was a regression originally introduced in
5e61ca9 ("During installation, add administrator user using criticctl")

installation/admin.py
@@ -108,12 +108,16 @@ def install(data):
     global password
 
     try:
-        subprocess.check_output(
-            [installation.criticctl.criticctl_path, "adduser",
-             "--name", username,
-             "--email", email,
-             "--fullname", fullname,
-             "--password", password])
+        criticctl_argv = [installation.criticctl.criticctl_path, "adduser",
+                          "--name", username,
+                          "--email", email,
+                          "--fullname", fullname]
+        if not password:
+            criticctl_argv.extend(["--no-password"])
+        else:
+            criticctl_argv.extend(["--password", password])
+
+        subprocess.check_output(criticctl_argv)
 
         for role in ["administrator", "repositories", "newswriter"]:
             subprocess.check_output(