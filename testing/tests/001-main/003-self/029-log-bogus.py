742c0a9304339698689e725dc48016c6cd5b7ba5
Martin Olsson <martin@minimum.se>
2014-03-20 09:52:37 +0100

    Add TC for "Report "Missing param 'repository'" when there is no repository"

---
+++ testing/tests/001-main/003-self/029-log-bogus.py
@@ -0,0 +1,14 @@
+expected = testing.expect.message("'notabranch' doesn't name a branch!", None)
+frontend.page(
+    url="log",
+    params={ "repository": "critic",
+             "branch": "notabranch" },
+    expect={ "message": expected })
+
+
+expected = testing.expect.message("Missing URI Parameter!",
+                                  "Expected 'repository' parameter.")
+frontend.page(
+    url="log",
+    params={ "branch": "branch_that_does_not_exist" },
+    expect={ "message": expected })
