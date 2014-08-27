jensl/critic

commit 6ef0eed59b61913635baf35cb9468250b6e08546

Common up calls to index.processCommits()
The processCommits() function inserts a commit and all its ancestors into
the 'commits' table in the database, if they aren't already there.  It
needs to be called before running any code that expects to be able to
access commits as gitutils.Commit objects.

It was previously both from src/background/githook.py and from various
functions in src/index.py (e.g. createBranch() and updateBranch().)  Now
it's simply called on all new SHA-1s that the commit hook receives as
input.  This is effectively what it did before as well, of course, but
with less code.

Also, since processCommits() can raise IndexException, and the handler
for that exception uses the 'info' local which is first assigned after
some of the calls to processCommits(), the exception handler would
sometimes crash, and not emit a proper error message.  This problem is
addressed by this fix.

src/index.py
@@ -178,8 +178,6 @@ def createBranches(user_name, repository_name, branches, flags):
     user = getUser(db, user_name)
     repository = gitutils.Repository.fromName(db, repository_name)
 
-    for name, head in branches: processCommits(repository_name, head)
-
     if len(branches) > 1:
         try:
             from customization.branches import compareBranchNames
@@ -231,8 +229,6 @@ def createBranches(user_name, repository_name, branches, flags):
     for name, head in branches: createBranch(user, repository, name, head, flags)
 
 def createBranch(user, repository, name, head, flags):
-    processCommits(repository.name, head)
-
     try:
         update(repository.path, "refs/heads/" + name, None, head)
     except Reject as rejected:
@@ -421,8 +417,6 @@ def createBranch(user, repository, name, head, flags):
 
 def updateBranch(user_name, repository_name, name, old, new, multiple, flags):
     repository = gitutils.Repository.fromName(db, repository_name)
-
-    processCommits(repository_name, new)
 
     try:
         update(repository.path, "refs/heads/" + name, old, new)
@@ -864,8 +858,6 @@ def createTag(repository_name, name, sha1):
     sha1 = gitutils.getTaggedCommit(repository, sha1)
 
     if sha1:
-        processCommits(repository.name, sha1)
-
         cursor = db.cursor()
         cursor.execute("INSERT INTO tags (name, repository, sha1) VALUES (%s, %s, %s)",
                        (name, repository.id, sha1))
@@ -877,8 +869,6 @@ def updateTag(repository_name, name, old_sha1, new_sha1):
     cursor = db.cursor()
 
     if sha1:
-        processCommits(repository.name, sha1)
-
         cursor.execute("UPDATE tags SET sha1=%s WHERE name=%s AND repository=%s",
                        (sha1, name, repository.id))
     else:

src/background/githook.py
@@ -65,13 +65,20 @@ def slave():
 
         index.init()
 
+        commits_to_process = set()
+
         for ref in request["refs"]:
             name = ref["name"]
             old_sha1 = ref["old_sha1"]
             new_sha1 = ref["new_sha1"]
 
-            if "//" in name: reject("invalid ref name: '%s'" % name)
-            if not name.startswith("refs/"): reject("unexpected ref name: '%s'" % name)
+            if "//" in name:
+                reject("invalid ref name: '%s'" % name)
+            if not name.startswith("refs/"):
+                reject("unexpected ref name: '%s'" % name)
+
+            if new_sha1 != '0000000000000000000000000000000000000000':
+                commits_to_process.add(new_sha1)
 
             name = name[len("refs/"):]
 
@@ -96,12 +103,14 @@ def slave():
                 name = name[len("temporary/"):]
                 if name != new_sha1:
                     reject("invalid update of '%s'; value is not %s" % (ref["name"], name))
-                index.processCommits(repository_name, new_sha1)
             else:
                 reject("unexpected ref name: '%s'" % ref["name"])
 
         multiple = (len(delete_branches) + len(update_branches) + len(create_branches) + len(delete_tags) + len(update_tags) + len(create_tags)) > 1
         info = []
+
+        for sha1 in commits_to_process:
+            index.processCommits(repository_name, sha1)
 
         for name, old in delete_branches:
             index.deleteBranch(user_name, repository_name, name, old)
@@ -178,6 +187,8 @@ class GitHookServer(background.utils.PeerServer):
 An exception was raised while processing the request.  A message has
 been sent to the system administrator(s).
 """)
+                if configuration.debug.IS_DEVELOPMENT:
+                    self.__client.write("\n" + result["error"].strip() + "\n")
             self.__client.close()
 
     class Client(background.utils.PeerServer.SocketPeer):

commit ced50ca2208c8440becbccdee7239994435c879a

Fix crash in pre-receive hook when pushing annotated tag
The value of an annotated tag is the SHA-1 of a tag object, not a commit
object.  Use "git rev-parse $sha1^{commit}" to resolve the SHA1s given to
index.processCommits() before using them as direct commit references.

Recent regression introduced by:

6ef0eed5 "Common up calls to index.processCommits()"

src/index.py
@@ -80,6 +80,8 @@ def processCommits(repository_name, sha1):
 
     if not repository: raise IndexException("No such repository: %r" % repository_name)
 
+    sha1 = repository.run("rev-parse", "--verify", "--quiet", sha1 + "^{commit}").strip()
+
     stack = []
     edges_values = []
 