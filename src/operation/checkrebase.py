jensl/critic

commit 1a9e4bf50fe88af7f23a773d9e5523668555430b

Remove dbutils.Branch.{head,tail,commits} attributes
Instead, add getHead(), getTail() and getCommits() methods (of which the
first already existed.)  These all raise an gitutils.GitReferenceError if
a commit they would have returned doesn't exist in the repository.

Also drop the loadCommits() method, and the (similar but different)
load_commits arguments to Branch.fromId() and Review.fromId().

Previously, the Branch.head attribute was set to None by Branch.fromId()
if load_commits was false, *or*, silently, if the commit was missing.  The
Branch.tail attribute, OTOH, was set to None by Branch.fromId() if
load_commits was false, but raised an exception if the commit was missing.

The load_commits argument, surprisingly, didn't mean the same as calling
loadCommits(); the latter set all of head, tail and commits, whereas the
former only set head and tail, leaving commits with a None value.

Also, the requirement that either load_commits=True be used, or that
loadCommits() has been called, before head, tail or commits are accessed
(to get non-None values) just leads to harder-to-follow and fragile code.
Better to have accessors that actually fetch the information if needed.

src/operation/checkrebase.py
@@ -30,7 +30,7 @@ class CheckMergeStatus(Operation):
 
     def process(self, db, user, review_id, new_head_sha1, new_upstream_sha1):
         review = dbutils.Review.fromId(db, review_id)
-        upstreams = log.commitset.CommitSet(review.branch.commits).getFilteredTails(review.repository)
+        upstreams = log.commitset.CommitSet(review.branch.getCommits(db)).getFilteredTails(review.repository)
 
         if len(upstreams) > 1:
             return OperationResult(rebase_supported=False)
@@ -80,7 +80,7 @@ class CheckConflictsStatus(Operation):
             if len(upstreams) > 1:
                 return OperationResult(rebase_supported=False)
 
-            old_head = review.branch.head
+            old_head = review.branch.getHead(db)
             old_upstream = gitutils.Commit.fromSHA1(db, review.repository, upstreams.pop())
             new_head = gitutils.Commit.fromSHA1(db, review.repository, new_head_sha1)
             new_upstream = gitutils.Commit.fromSHA1(db, review.repository, new_upstream_sha1)
@@ -122,7 +122,7 @@ class CheckHistoryRewriteStatus(Operation):
     def process(self, db, user, review_id, new_head_sha1):
         review = dbutils.Review.fromId(db, review_id)
 
-        old_head = review.branch.head
+        old_head = review.branch.getHead(db)
         new_head = gitutils.Commit.fromSHA1(db, review.repository, new_head_sha1)
 
         mergebase = review.repository.mergebase([old_head, new_head])

src/cli.py
@@ -104,7 +104,7 @@ try:
                 filters = reviewing.filters.Filters()
                 user = dbutils.User.fromId(db, data["user_id"]) if "user_id" in data else None
                 if "review_id" in data:
-                    review = dbutils.Review.fromId(db, data["review_id"], load_commits=False)
+                    review = dbutils.Review.fromId(db, data["review_id"])
                     filters.setFiles(db, review=review)
                     filters.load(db, review=review, user=user,
                                  added_review_filters=data.get("added_review_filters", []),

src/critic.py
@@ -348,7 +348,7 @@ def suggestreview(req, db, _user):
 
     def addSuggestions():
         for review_id, summary in cursor:
-            review = dbutils.Review.fromId(db, review_id, load_commits=False)
+            review = dbutils.Review.fromId(db, review_id)
             if review.state != 'dropped':
                 suggestions[str(review_id)] = "(%s) %s" % (review.getReviewState(db), summary)
 

src/dbutils/branch.py
@@ -15,18 +15,20 @@
 # the License.
 
 class Branch(object):
-    def __init__(self, id, repository, name, head, base, tail, branch_type, archived, review_id):
+    def __init__(self, id, repository, name, head_sha1, base, tail_sha1, branch_type, archived, review_id):
         self.id = id
         self.repository = repository
         self.name = name
-        self.head = head
+        self.head_sha1 = head_sha1
         self.base = base
-        self.tail = tail
+        self.tail_sha1 = tail_sha1
         self.type = branch_type
         self.archived = archived
         self.review_id = review_id
         self.review = None
-        self.commits = None
+        self.__commits = None
+        self.__head = None
+        self.__tail = None
 
     def __eq__(self, other):
         return self.id == other.id
@@ -45,16 +47,15 @@ class Branch(object):
 
     def getHead(self, db):
         import gitutils
-        if not self.head:
-            cursor = db.cursor()
-            cursor.execute("""SELECT commits.id, commits.sha1
-                                FROM commits
-                                JOIN branches ON (commits.id=branches.head)
-                               WHERE branches.id=%s""",
-                           (self.id,))
-            head_id, head_sha1 = cursor.fetchone()
-            self.head = gitutils.Commit.fromSHA1(db, self.repository, head_sha1, head_id)
-        return self.head
+        if not self.__head:
+            self.__head = gitutils.Commit.fromSHA1(db, self.repository, self.head_sha1)
+        return self.__head
+
+    def getTail(self, db):
+        import gitutils
+        if not self.__tail:
+            self.__tail = gitutils.Commit.fromSHA1(db, self.repository, self.tail_sha1)
+        return self.__tail
 
     def getJSConstructor(self):
         from htmlutils import jsify
@@ -66,22 +67,18 @@ class Branch(object):
     def getJS(self):
         return "var branch = %s;" % self.getJSConstructor()
 
-    def loadCommits(self, db):
+    def getCommits(self, db):
         import gitutils
-        if self.commits is None:
+        if self.__commits is None:
             cursor = db.cursor()
-            cursor.execute("SELECT commits.id, commits.sha1 FROM reachable, commits WHERE reachable.branch=%s AND reachable.commit=commits.id", [self.id])
-            self.commits = []
-            for commit_id, sha1 in cursor:
-                self.commits.append(gitutils.Commit.fromSHA1(db, self.repository, sha1, commit_id=commit_id))
-            cursor.execute("SELECT commits.id, commits.sha1 FROM branches, commits WHERE branches.id=%s AND branches.head=commits.id", [self.id])
-            commit_id, sha1 = cursor.fetchone()
-            self.head = gitutils.Commit.fromSHA1(db, self.repository, sha1, commit_id=commit_id)
-            cursor.execute("SELECT commits.id, commits.sha1 FROM branches, commits WHERE branches.id=%s AND branches.tail=commits.id", [self.id])
-            row = cursor.fetchone()
-            if row:
-                commit_id, sha1 = row
-                self.tail = gitutils.Commit.fromSHA1(db, self.repository, sha1, commit_id=commit_id)
+            cursor.execute("""SELECT commits.id, commits.sha1
+                                FROM reachable
+                                JOIN commits ON (commits.id=reachable.commit)
+                               WHERE reachable.branch=%s""",
+                           (self.id,))
+            self.__commits = [gitutils.Commit.fromSHA1(db, self.repository, sha1, commit_id=commit_id)
+                              for commit_id, sha1 in cursor]
+        return self.__commits
 
     def rebase(self, db, base):
         import gitutils
@@ -128,12 +125,10 @@ class Branch(object):
         old_count = cursor.fetchone()[0]
 
         if base.base and base.base.id == self.id:
-            self.loadCommits(db)
-
             cursor.execute("SELECT count(*) FROM reachable WHERE branch=%s", (base.id,))
             base_old_count = cursor.fetchone()[0]
 
-            base_reachable = findReachable(base.head, self.base.id, set([commit.sha1 for commit in self.commits]))
+            base_reachable = findReachable(base.getHead(db), self.base.id, set(commit.sha1 for commit in self.getCommits(db)))
             base_new_count = len(base_reachable)
 
             cursor.execute("DELETE FROM reachable WHERE branch=%s", [base.id])
@@ -141,12 +136,12 @@ class Branch(object):
             cursor.execute("UPDATE branches SET base=%s WHERE id=%s", [self.base.id, base.id])
 
             base.base = self.base
-            base.commits = None
+            base.__commits = None
         else:
             base_old_count = None
             base_new_count = None
 
-        our_reachable = findReachable(self.head, base.id)
+        our_reachable = findReachable(self.getHead(db), base.id)
         new_count = len(our_reachable)
 
         cursor.execute("DELETE FROM reachable WHERE branch=%s", [self.id])
@@ -154,7 +149,7 @@ class Branch(object):
         cursor.execute("UPDATE branches SET base=%s WHERE id=%s", [base.id, self.id])
 
         self.base = base
-        self.commits = None
+        self.__commits = None
 
         return old_count, new_count, base_old_count, base_new_count
 
@@ -203,7 +198,7 @@ class Branch(object):
         self.archived = False
 
     @staticmethod
-    def fromId(db, branch_id, load_review=False, load_commits=True, repository=None, for_update=False, profiler=None):
+    def fromId(db, branch_id, load_review=False, repository=None, for_update=False, profiler=None):
         import gitutils
 
         cursor = db.cursor()
@@ -218,6 +213,14 @@ class Branch(object):
         else:
             branch_name, repository_id, head_commit_id, base_branch_id, tail_commit_id, type, archived, review_id = row
 
+            def commit_sha1(commit_id):
+                cursor.execute("SELECT sha1 FROM commits WHERE id=%s", (commit_id,))
+                return cursor.fetchone()[0]
+
+            head_commit_sha1 = commit_sha1(head_commit_id)
+            tail_commit_sha1 = (commit_sha1(tail_commit_id)
+                                if tail_commit_id is not None else None)
+
             if profiler: profiler.check("Branch.fromId: basic")
 
             if repository is None:
@@ -227,31 +230,12 @@ class Branch(object):
 
             if profiler: profiler.check("Branch.fromId: repository")
 
-            if load_commits:
-                try:
-                    head_commit = gitutils.Commit.fromId(db, repository, head_commit_id)
-                except Exception:
-                    head_commit = None
+            base_branch = (Branch.fromId(db, base_branch_id, repository=repository)
+                           if base_branch_id is not None else None)
 
-                if profiler: profiler.check("Branch.fromId: head")
-            else:
-                head_commit = None
+            if profiler: profiler.check("Branch.fromId: base")
 
-            if load_commits:
-                base_branch = (Branch.fromId(db, base_branch_id)
-                               if base_branch_id is not None else None)
-
-                if profiler: profiler.check("Branch.fromId: base")
-
-                tail_commit = (gitutils.Commit.fromId(db, repository, tail_commit_id)
-                               if tail_commit_id is not None else None)
-
-                if profiler: profiler.check("Branch.fromId: tail")
-            else:
-                base_branch = None
-                tail_commit = None
-
-            branch = Branch(branch_id, repository, branch_name, head_commit, base_branch, tail_commit, type, archived, review_id)
+            branch = Branch(branch_id, repository, branch_name, head_commit_sha1, base_branch, tail_commit_sha1, type, archived, review_id)
 
             if load_review:
                 from dbutils import Review

src/dbutils/review.py
@@ -221,7 +221,6 @@ class Review(object):
         self.description = description
         self.reviewers = []
         self.watchers = {}
-        self.changesets = []
         self.commentchains = None
         self.applyfilters = applyfilters
         self.applyparentfilters = applyparentfilters
@@ -353,7 +352,7 @@ class Review(object):
             return True
 
         if include_head_and_tails:
-            head_and_tails = set([self.branch.head])
+            head_and_tails = set([self.branch.getHead(db)])
 
             commitset = self.getCommitSet(db)
 
@@ -600,9 +599,9 @@ class Review(object):
         cursor.execute("SELECT uid, path, type, NULL FROM reviewfilters WHERE review=%s", (self.id,))
         return cursor.fetchall() or None
 
-    def getFilteredTails(self):
+    def getFilteredTails(self, db):
         import log.commitset
-        commitset = log.commitset.CommitSet(self.branch.commits)
+        commitset = log.commitset.CommitSet(self.branch.getCommits(db))
         return commitset.getFilteredTails(self.branch.repository)
 
     def getRelevantFiles(self, db, user):
@@ -655,7 +654,7 @@ class Review(object):
         return ", ".join(association)
 
     @staticmethod
-    def fromId(db, review_id, branch=None, load_commits=True, profiler=None):
+    def fromId(db, review_id, branch=None, profiler=None):
         from dbutils import User
 
         cursor = db.cursor()
@@ -669,7 +668,7 @@ class Review(object):
 
         if branch is None:
             from dbutils import Branch
-            branch = Branch.fromId(db, branch_id, load_review=False, load_commits=load_commits, profiler=profiler)
+            branch = Branch.fromId(db, branch_id, load_review=False, profiler=profiler)
 
         cursor.execute("SELECT uid FROM reviewusers WHERE review=%s AND owner", (review_id,))
 
@@ -704,19 +703,6 @@ class Review(object):
             review.watchers[watcher] = watcher_types[watcher]
 
         if profiler: profiler.check("Review.fromId: users")
-
-        if load_commits:
-            review.branch.loadCommits(db)
-
-            cursor.execute("""SELECT id
-                                FROM reviewchangesets
-                                JOIN changesets ON (id=changeset)
-                               WHERE review=%s
-                                 AND child=ANY (%s)""", (review_id, [commit.id for commit in review.branch.commits]))
-
-            review.changesets = [changeset_id for (changeset_id,) in cursor.fetchall()]
-
-            if profiler: profiler.check("Review.fromId: load commits")
 
         return review
 

src/gitutils.py
@@ -199,7 +199,7 @@ class Repository:
         import dbutils
         if not self.__main_branch:
             if self.__main_branch_id is not None:
-                self.__main_branch = dbutils.Branch.fromId(db, self.__main_branch_id, load_commits=False)
+                self.__main_branch = dbutils.Branch.fromId(db, self.__main_branch_id)
         return self.__main_branch
 
     @staticmethod

src/index.py
@@ -474,8 +474,8 @@ def updateBranch(user_name, repository_name, name, old, new, multiple, flags):
             raise IndexException("The branch '%s' is not in the database!" % name)
         base_branch_id = branch.base.id if branch.base else None
 
-    if branch.head.sha1 != old:
-        if new == branch.head.sha1:
+    if branch.head_sha1 != old:
+        if new == branch.head_sha1:
             # This is what we think the ref ought to be already.  Do nothing,
             # and let the repository "catch up."
             return
@@ -483,7 +483,7 @@ def updateBranch(user_name, repository_name, name, old, new, multiple, flags):
             data = { "name": name,
                      "old": old[:8],
                      "new": new[:8],
-                     "current": branch.head.sha1[:8] }
+                     "current": branch.head_sha1[:8] }
 
             message = """CONFUSED!  Git thinks %(name)s points to %(old)s, but Critic thinks it points to %(current)s.  Rejecting push since it would only makes matters worse.  To resolve this problem, use
 
@@ -518,8 +518,8 @@ Please don't push it manually to this repository.""" % (name, remote_name, remot
         assert updating
 
         if not name.startswith("r/"):
-            conflicting = repository.revlist([branch.head.sha1], [new])
-            added = repository.revlist([new], [branch.head.sha1])
+            conflicting = repository.revlist([branch.head_sha1], [new])
+            added = repository.revlist([new], [branch.head_sha1])
 
             if conflicting:
                 if forced:
@@ -614,7 +614,7 @@ This review is currently being rebased by
 and can't be otherwise updated right now.""" % (rebaser.fullname, rebaser.email))
 
             old_head = gitutils.Commit.fromId(db, repository, old_head_id)
-            old_commitset = log.commitset.CommitSet(review.branch.commits)
+            old_commitset = log.commitset.CommitSet(review.branch.getCommits(db))
 
             if old_head.sha1 != old:
                 raise IndexException("""\
@@ -721,7 +721,7 @@ the new upstream specified and then push that instead.""")
                 cursor.execute("UPDATE branches SET head=%s WHERE id=%s",
                                (new_head.getId(db), review.branch.id))
             else:
-                old_commitset = log.commitset.CommitSet(review.branch.commits)
+                old_commitset = log.commitset.CommitSet(review.branch.getCommits(db))
                 new_sha1s = repository.revlist([new], old_commitset.getTails(), '--topo-order')
 
                 if old_head.sha1 in new_sha1s:
@@ -795,9 +795,22 @@ first, and then repeat this push.""" % name)
     root_branch_id = cursor.fetchone()[0]
 
     def isreachable(sha1):
-        if is_review and sha1 == branch.tail: return True
-        if base_branch_id: cursor.execute("SELECT 1 FROM commits, reachable WHERE commits.sha1=%s AND commits.id=reachable.commit AND reachable.branch IN (%s, %s, %s)", [sha1, branch.id, base_branch_id, root_branch_id])
-        else: cursor.execute("SELECT 1 FROM commits, reachable WHERE commits.sha1=%s AND commits.id=reachable.commit AND reachable.branch IN (%s, %s)", [sha1, branch.id, root_branch_id])
+        if is_review and sha1 == branch.tail_sha1:
+            return True
+        if base_branch_id:
+            cursor.execute("""SELECT 1
+                                FROM commits
+                                JOIN reachable ON (reachable.commit=commits.id)
+                               WHERE commits.sha1=%s
+                                 AND reachable.branch IN (%s, %s, %s)""",
+                           (sha1, branch.id, base_branch_id, root_branch_id))
+        else:
+            cursor.execute("""SELECT 1
+                                FROM commits
+                                JOIN reachable ON (reachable.commit=commits.id)
+                               WHERE commits.sha1=%s
+                                 AND reachable.branch IN (%s, %s)""",
+                           (sha1, branch.id, root_branch_id))
         return cursor.fetchone() is not None
 
     stack = [new]

src/log/html.py
@@ -125,9 +125,8 @@ def render(db, target, title, branch=None, commits=None, columns=DEFAULT_COLUMNS
 
     if branch is not None:
         repository = branch.repository
-        branch.loadCommits(db)
-        commits = branch.commits[:]
-        commit_set = log.commitset.CommitSet(branch.commits)
+        commits = branch.getCommits(db)[:]
+        commit_set = log.commitset.CommitSet(commits)
     else:
         assert commits is not None
         repository = commits[0].repository if len(commits) else None
@@ -459,8 +458,7 @@ def render(db, target, title, branch=None, commits=None, columns=DEFAULT_COLUMNS
         cell = row.td(colspan=len(columns), align='center')
         cell.text("No commits. ")
         if review:
-            review.branch.loadCommits(db)
-            cell.a(href="showtree?sha1=%s&review=%d" % (review.branch.head.sha1, review.id)).text("[Browse tree]")
+            cell.a(href="showtree?sha1=%s&review=%d" % (review.branch.head_sha1, review.id)).text("[Browse tree]")
         return
     elif len(heads) > 1:
         error_message = "Invalid commit set: Multiple heads."

commit bd79c50cfc5741ea2b67eca12241b1911280d968

Fix crash in /checkmergestatus operation
This fixes a regression introduced by

  1a9e4bf5 "Remove dbutils.Branch.{head,tail,commits} attributes"

which forgot to change this one use of Branch.head to use getHead().

src/operation/checkrebase.py
@@ -35,7 +35,7 @@ class CheckMergeStatus(Operation):
         if len(upstreams) > 1:
             return OperationResult(rebase_supported=False)
 
-        old_head = review.branch.head
+        old_head = review.branch.getHead(db)
         old_upstream = gitutils.Commit.fromSHA1(db, review.repository, upstreams.pop())
         new_head = gitutils.Commit.fromSHA1(db, review.repository, new_head_sha1)
         new_upstream = gitutils.Commit.fromSHA1(db, review.repository, new_upstream_sha1)