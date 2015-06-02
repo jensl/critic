jensl/critic

commit 0103797d252e133d9b50479375dd272a38124609

Clean up handling of equivalent merges and replayed rebases
When performing a move rebase, we will either create an "equivalent merge"
(for a fast-forward rebase) or a "replayed rebase" (otherwise.)  In the
former case, we'd update the review rebase's "old head" to point to the
equivalent merge, as if it had been pushed to the review before the rebase.
A replayed rebase wasn't directly recorded in the database at all, instead
its existence was deduced from there being a changeset added to the review
of type "conflicts" whose child commit was the new head of the rebase.

This was generally messy, and error-prone to work with.

Instead of all this, add two explicit columns to the reviewrebases table,
equivalent_merge and replayed_rebase, and set the appropriate one to point
to the generated commit when performing a rebase.  Adjust code elsewhere
to use the values of these columns.

src/index.py
@@ -642,6 +642,11 @@ the new upstream specified and then push that instead.""")
 
                     repository.keepalive(old_head)
                     repository.keepalive(replayed_rebase)
+
+                    cursor.execute("""UPDATE reviewrebases
+                                         SET replayed_rebase=%s
+                                       WHERE id=%s""",
+                                   (replayed_rebase.getId(db), rebase_id))
                 else:
                     reviewing.utils.addCommitsToReview(
                         db, user, review, [merge], pending_mails=pending_mails,
@@ -649,16 +654,16 @@ the new upstream specified and then push that instead.""")
 
                     repository.keepalive(merge)
 
-                if not unrelated_move:
                     cursor.execute("""UPDATE reviewrebases
-                                         SET old_head=%s
-                                       WHERE review=%s AND new_head IS NULL""",
-                                   (merge.id, review.id))
+                                         SET equivalent_merge=%s
+                                       WHERE id=%s""",
+                                   (merge.getId(db), rebase_id))
 
                 cursor.execute("""UPDATE reviewrebases
-                                     SET new_head=%s, new_upstream=%s
-                                   WHERE review=%s AND new_head IS NULL""",
-                               (new_head.getId(db), new_upstream.getId(db), review.id))
+                                     SET new_head=%s,
+                                         new_upstream=%s
+                                   WHERE id=%s""",
+                               (new_head.getId(db), new_upstream.getId(db), rebase_id))
 
                 cursor.execute("""INSERT INTO previousreachable (rebase, commit)
                                        SELECT %s, commit
@@ -712,8 +717,8 @@ the rebased local branch to see what those changes are.""")
 
                 cursor.execute("""UPDATE reviewrebases
                                      SET new_head=%s
-                                   WHERE review=%s AND new_head IS NULL""",
-                               (new_head.getId(db), review.id))
+                                   WHERE id=%s""",
+                               (new_head.getId(db), rebase_id))
 
                 cursor.execute("""INSERT INTO previousreachable (rebase, commit)
                                        SELECT %s, commit

src/api/impl/review.py
@@ -125,13 +125,12 @@ class Review(object):
                      FROM commits
                      JOIN changesets ON (changesets.child=commits.id)
                      JOIN reviewchangesets ON (reviewchangesets.changeset=changesets.id)
-          LEFT OUTER JOIN reviewrebases ON (reviewrebases.old_head=commits.id
-                                        AND reviewrebases.new_head IS NOT NULL
-                                        AND reviewrebases.new_upstream IS NOT NULL)
+          LEFT OUTER JOIN reviewrebases ON (reviewrebases.review=%s
+                                        AND reviewrebases.equivalent_merge=commits.id)
                     WHERE reviewchangesets.review=%s
                       AND changesets.type='merge'
                       AND reviewrebases.id IS NULL""",
-                (self.id,))
+                (self.id, self.id))
             commit_ids_sha1s.update(cursor)
             repository = self.getRepository(critic)
             commits = [api.commit.fetch(repository, commit_id, sha1)
@@ -144,7 +143,8 @@ class Review(object):
             critic = wrapper.critic
             cursor = critic.getDatabaseCursor()
             cursor.execute(
-                """SELECT id, old_head, new_head, old_upstream, new_upstream, uid
+                """SELECT id, old_head, new_head, old_upstream, new_upstream,
+                          equivalent_merge, replayed_rebase, uid
                      FROM reviewrebases
                     WHERE review=%s
                       AND new_head IS NOT NULL

installation/data/dbschema.sql
@@ -340,6 +340,8 @@ CREATE TABLE reviewrebases
     new_head INTEGER REFERENCES commits,
     old_upstream INTEGER REFERENCES commits,
     new_upstream INTEGER REFERENCES commits,
+    equivalent_merge INTEGER REFERENCES commits,
+    replayed_rebase INTEGER REFERENCES commits,
     uid INTEGER NOT NULL REFERENCES users,
     branch VARCHAR(256),
 

installation/migrations/dbschema.altertable.reviewrebases.add.equivalent_merge.py
@@ -1,0 +1,133 @@
+# -*- mode: python; encoding: utf-8 -*-
+#
+# Copyright 2014 the Critic contributors, Opera Software ASA
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License.  You may obtain a copy of
+# the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+# License for the specific language governing permissions and limitations under
+# the License.
+
+import sys
+import psycopg2
+import argparse
+import os
+import subprocess
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--uid", type=int)
+parser.add_argument("--gid", type=int)
+
+arguments = parser.parse_args()
+
+os.setgid(arguments.gid)
+os.setuid(arguments.uid)
+
+import configuration
+
+db = psycopg2.connect(**configuration.database.PARAMETERS)
+cursor = db.cursor()
+
+try:
+    # Check if the 'equivalent_merge' column already exists.
+    cursor.execute("SELECT equivalent_merge FROM reviewrebases")
+except psycopg2.ProgrammingError:
+    db.rollback()
+else:
+    # No error; change appears to have been made already.
+    db.close()
+    sys.exit(0)
+
+def fetch_commit_sha1(commit_id):
+    cursor.execute("SELECT sha1 FROM commits WHERE id=%s", (commit_id,))
+    (sha1,) = cursor.fetchone()
+    return sha1
+
+def get_parent_sha1s(repository_path, sha1):
+    output = subprocess.check_output(
+        [configuration.executables.GIT, "log", "-1", "--format=%P", sha1],
+        cwd=repository_path)
+    return output.strip().split()
+
+def is_ancestor_of(repository_path, ancestor_sha1, descendant_sha1):
+    try:
+        merge_base_sha1 = subprocess.check_output(
+            [configuration.executables.GIT, "merge-base",
+             ancestor_sha1, descendant_sha1],
+            cwd=repository_path).strip()
+    except subprocess.CalledProcessError:
+        return False
+    else:
+        return merge_base_sha1 == ancestor_sha1
+
+cursor.execute("""ALTER TABLE reviewrebases
+                          ADD equivalent_merge INTEGER REFERENCES commits""")
+
+#
+# Move all references to equivalent merges stored in the |old_head| column of
+# existing review rebases over to the new |equivalent_merge| column, and restore
+# the value of the |old_head| to be the actual head of the review branch before
+# the rebase.
+#
+
+cursor.execute("""SELECT repositories.path,
+                         reviewrebases.id,
+                         reviewrebases.old_head,
+                         reviewrebases.old_upstream,
+                         reviewrebases.new_upstream
+                    FROM reviewrebases
+                    JOIN reviews ON (reviews.id=reviewrebases.review)
+                    JOIN branches ON (branches.id=reviews.branch)
+                    JOIN repositories ON (repositories.id=branches.repository)
+                   WHERE new_head IS NOT NULL
+                     AND old_upstream IS NOT NULL
+                     AND new_upstream IS NOT NULL""")
+
+for repository_path, rebase_id, old_head_id, old_upstream_id, new_upstream_id in cursor.fetchall():
+    old_head_sha1 = fetch_commit_sha1(old_head_id)
+    old_head_parent_sha1s = get_parent_sha1s(repository_path, old_head_sha1)
+
+    if len(old_head_parent_sha1s) != 2:
+        # Old head is not a merge commit (or is an 3-or-more-way merge,) so
+        # can't be an equivalent merge.
+        continue
+
+    old_upstream_sha1 = fetch_commit_sha1(old_upstream_id)
+    new_upstream_sha1 = fetch_commit_sha1(new_upstream_id)
+
+    if old_head_parent_sha1s[1] != new_upstream_sha1:
+        # An equivalent merge should be a merge with the real old head as the
+        # first parent and the new upstream as the second parent.  We can't
+        # really check the first parent in a meaningful way, but if the second
+        # parent is "wrong", then this can't be an equivalent merge.
+        continue
+
+    if not is_ancestor_of(repository_path, old_upstream_sha1, new_upstream_sha1):
+        # Old upstream is not an ancestor of the new upstream, meaning this is
+        # not a "fast-forward" rebase.  Such rebases don't have an equivalent
+        # merge, but rather a "replayed rebase".  The replayed rebase however
+        # isn't stored in the |old_head| column, so there is nothing to restore.
+        continue
+
+    # Alright, we're pretty sure that the old head is in fact an equivalent
+    # merge commit.  Store it in the new |equivalent_merge| column and restore
+    # the |old_head| column to the equivalent merge's first parent.
+
+    cursor.execute("SELECT id FROM commits WHERE sha1=%s",
+                   (old_head_parent_sha1s[0],))
+    (real_old_head_id,) = cursor.fetchone()
+
+    cursor.execute("""UPDATE reviewrebases
+                         SET old_head=%s,
+                             equivalent_merge=%s
+                       WHERE id=%s""",
+                   (real_old_head_id, old_head_id, rebase_id))
+
+db.commit()
+db.close()

installation/migrations/dbschema.altertable.reviewrebases.add.replayed_rebase.py
@@ -1,0 +1,78 @@
+# -*- mode: python; encoding: utf-8 -*-
+#
+# Copyright 2014 the Critic contributors, Opera Software ASA
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License.  You may obtain a copy of
+# the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+# License for the specific language governing permissions and limitations under
+# the License.
+
+import sys
+import psycopg2
+import argparse
+import os
+import subprocess
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--uid", type=int)
+parser.add_argument("--gid", type=int)
+
+arguments = parser.parse_args()
+
+os.setgid(arguments.gid)
+os.setuid(arguments.uid)
+
+import configuration
+
+db = psycopg2.connect(**configuration.database.PARAMETERS)
+cursor = db.cursor()
+
+try:
+    # Check if the 'replayed_rebase' column already exists.
+    cursor.execute("SELECT replayed_rebase FROM reviewrebases")
+except psycopg2.ProgrammingError:
+    db.rollback()
+else:
+    # No error; change appears to have been made already.
+    db.close()
+    sys.exit(0)
+
+cursor.execute("""ALTER TABLE reviewrebases
+                          ADD replayed_rebase INTEGER REFERENCES commits""")
+
+#
+# Find all replayed rebases and store them in the new |replayed_rebase| column.
+# We identify them via 'conflicts' changesets added for review, whose child
+# (right-hand side) is the new head of a rebase.  The parent (left-hand side) of
+# such a changeset will be the replayed rebase commit.
+#
+# Note: It is theoretically possible for such a 'conflicts' changeset to exist
+# that is not actually indicative of a replayed rebase, if the rebase's new head
+# is a merge commit, and that merge commit is an equivalent merge commit created
+# for an earlier rebase of the same review.
+#
+# Also note: While theoretically possible, the aforementioned possibility is not
+# likely to have happened in practice.
+#
+
+cursor.execute("""SELECT DISTINCT changesets.parent, reviewrebases.id
+                    FROM reviewrebases
+                    JOIN reviewchangesets ON (reviewchangesets.review=reviewrebases.review)
+                    JOIN changesets ON (changesets.id=reviewchangesets.changeset
+                                    AND changesets.child=reviewrebases.new_head)
+                   WHERE changesets.type='conflicts'""")
+
+cursor.executemany("""UPDATE reviewrebases
+                         SET replayed_rebase=%s
+                       WHERE id=%s""",
+                   cursor.fetchall())
+
+db.commit()
+db.close()

commit 31a73931748db6eb7966c23cf57d2724d8cbcb24

Fix comment propagation past fast-forward move rebases
Adjust ReviewRebases.fromOldHead() to handle the case that the "old head"
is an equivalent merge commit.  It used to, since equivalent merge commits
used to actually be the old heads of their rebases.  When that was changed,
comment propagation, the only user of fromOldHead(), was inadvertently
affected (broken, in fact.)

This fixes a regression introduced by

  0103797d "Clean up handling of equivalent merges and replayed rebases"

src/dbutils/review.py
@@ -150,13 +150,16 @@ class ReviewState(object):
             else: return progress
 
 class ReviewRebase(object):
-    def __init__(self, review, old_head, new_head, old_upstream, new_upstream, user):
+    def __init__(self, review, old_head, new_head, old_upstream, new_upstream, user,
+                 equivalent_merge, replayed_rebase):
         self.review = review
         self.old_head = old_head
         self.new_head = new_head
         self.old_upstream = old_upstream
         self.new_upstream = new_upstream
         self.user = user
+        self.equivalent_merge = equivalent_merge
+        self.replayed_rebase = replayed_rebase
 
 class ReviewRebases(list):
     def __init__(self, db, review):
@@ -167,13 +170,15 @@ class ReviewRebases(list):
         self.__new_head_map = {}
 
         cursor = db.cursor()
-        cursor.execute("""SELECT old_head, new_head, old_upstream, new_upstream, uid
+        cursor.execute("""SELECT old_head, new_head, old_upstream, new_upstream, uid,
+                                 equivalent_merge, replayed_rebase
                             FROM reviewrebases
                            WHERE review=%s
                              AND new_head IS NOT NULL""",
                        (review.id,))
 
-        for old_head_id, new_head_id, old_upstream_id, new_upstream_id, user_id in cursor:
+        for (old_head_id, new_head_id, old_upstream_id, new_upstream_id, user_id,
+             equivalent_merge_id, replayed_rebase_id) in cursor:
             old_head = gitutils.Commit.fromId(db, review.repository, old_head_id)
             new_head = gitutils.Commit.fromId(db, review.repository, new_head_id)
 
@@ -183,12 +188,26 @@ class ReviewRebases(list):
             else:
                 old_upstream = new_upstream = None
 
+            if equivalent_merge_id:
+                equivalent_merge = gitutils.Commit.fromId(db, review.repository, equivalent_merge_id)
+            else:
+                equivalent_merge = None
+
+            if replayed_rebase_id:
+                replayed_rebase = gitutils.Commit.fromId(db, review.repository, replayed_rebase_id)
+            else:
+                replayed_rebase = None
+
             user = User.fromId(db, user_id)
-            rebase = ReviewRebase(review, old_head, new_head, old_upstream, new_upstream, user)
+            rebase = ReviewRebase(review, old_head, new_head, old_upstream, new_upstream, user,
+                                  equivalent_merge, replayed_rebase)
 
             self.append(rebase)
             self.__old_head_map[old_head] = rebase
             self.__new_head_map[new_head] = rebase
+
+            if equivalent_merge:
+                self.__old_head_map[equivalent_merge] = rebase
 
         if review.performed_rebase:
             self.__old_head_map[review.performed_rebase.old_head] = review.performed_rebase
@@ -272,8 +291,10 @@ class Review(object):
 
         return ReviewState(self, self.accepted(db), pending, reviewed, issues)
 
-    def setPerformedRebase(self, old_head, new_head, old_upstream, new_upstream, user):
-        self.performed_rebase = ReviewRebase(self, old_head, new_head, old_upstream, new_upstream, user)
+    def setPerformedRebase(self, old_head, new_head, old_upstream, new_upstream, user,
+                           equivalent_merge, replayed_rebase):
+        self.performed_rebase = ReviewRebase(self, old_head, new_head, old_upstream, new_upstream, user,
+                                             equivalent_merge, replayed_rebase)
 
     def getReviewRebases(self, db):
         return ReviewRebases(db, self)

src/index.py
@@ -603,12 +603,14 @@ the new upstream specified and then push that instead.""")
                 if not old_upstream.isAncestorOf(new_upstream):
                     unrelated_move = True
 
+                equivalent_merge = replayed_rebase = None
+
                 if unrelated_move:
                     replayed_rebase = reviewing.rebase.replayRebase(
                         db, review, user, old_head, old_upstream, new_head,
                         new_upstream, onto_branch)
                 else:
-                    merge = reviewing.rebase.createEquivalentMergeCommit(
+                    equivalent_merge = reviewing.rebase.createEquivalentMergeCommit(
                         db, review, user, old_head, old_upstream, new_head,
                         new_upstream, onto_branch)
 
@@ -626,7 +628,8 @@ the new upstream specified and then push that instead.""")
 
                 print "Rebase performed."
 
-                review.setPerformedRebase(old_head, new_head, old_upstream, new_upstream, user)
+                review.setPerformedRebase(old_head, new_head, old_upstream, new_upstream, user,
+                                          equivalent_merge, replayed_rebase)
 
                 if unrelated_move:
                     reviewing.utils.addCommitsToReview(
@@ -644,15 +647,15 @@ the new upstream specified and then push that instead.""")
                                    (replayed_rebase.getId(db), rebase_id))
                 else:
                     reviewing.utils.addCommitsToReview(
-                        db, user, review, [merge], pending_mails=pending_mails,
-                        silent_if_empty=set([merge]), full_merges=set([merge]))
+                        db, user, review, [equivalent_merge], pending_mails=pending_mails,
+                        silent_if_empty=set([equivalent_merge]), full_merges=set([equivalent_merge]))
 
-                    repository.keepalive(merge)
+                    repository.keepalive(equivalent_merge)
 
                     cursor.execute("""UPDATE reviewrebases
                                          SET equivalent_merge=%s
                                        WHERE id=%s""",
-                                   (merge.getId(db), rebase_id))
+                                   (equivalent_merge.getId(db), rebase_id))
 
                 cursor.execute("""UPDATE reviewrebases
                                      SET new_head=%s,

testing/tests/001-main/003-self/033-propagation-vs-rebase.py
@@ -1,0 +1,186 @@
+import os
+
+FILENAME = "033-propagation-vs-rebase.txt"
+FILENAME_BASE = "033-propagation-vs-rebase.base.txt"
+
+NONSENSE = """\
+Lorem ipsum dolor sit amet, consectetur adipiscing
+elit. Donec ut enim sit amet purus ultricies
+lobortis. Pellentesque nisi arcu, convallis sed purus sed,
+semper ultrices velit. Ut egestas lorem tortor, vitae
+lacinia lorem consectetur nec. Integer tempor ornare ipsum
+at viverra. Curabitur nec orci mollis, lacinia sapien eget,
+ultricies ipsum. Curabitur a libero tortor. Curabitur
+volutpat lacinia erat, ac suscipit enim dignissim nec."""
+
+def lines(*args):
+    return "\n".join((line.upper() if index in args else line)
+                     for index, line in enumerate(NONSENSE.splitlines()))
+
+def to(name):
+    return testing.mailbox.ToRecipient("%s@example.org" % name)
+
+def about(subject):
+    return testing.mailbox.WithSubject(subject)
+
+SETTINGS = { "review.createViaPush": True,
+             "email.subjectLine.updatedReview.reviewRebased":
+                 "Rebased Review: %(summary)s" }
+
+work = repository.workcopy()
+settings = testing.utils.settings("alice", SETTINGS)
+signin = frontend.signin("alice")
+reviews = []
+
+with work, settings, signin:
+    REMOTE_URL = instance.repository_url("alice")
+
+    def write(*args, **kwargs):
+        """Write the file<tm>, optionally with lines upper-cased."""
+        filename = kwargs.get("filename", FILENAME)
+        with open(os.path.join(work.path, filename), "w") as target:
+            print >>target, lines(*args)
+
+    def commit(message, *args, **kwargs):
+        """Create a commit and return its SHA-1"""
+        write(*args, **kwargs)
+        work.run(["add", kwargs.get("filename", FILENAME)])
+        work.run(["commit", "-m", message])
+        return work.run(["rev-parse", "HEAD"]).strip()
+
+    def expectmail(title):
+        review = reviews[-1]
+        mailbox.pop(accept=[to("alice"),
+                            about("%s: %s" % (title, review["summary"]))])
+
+    def expecthead(expected):
+        """Check that the review branch in Critic's repository is where we want
+           it to be."""
+        review = reviews[-1]
+        actual = work.run(["ls-remote", REMOTE_URL, review["branch"]]).split()[0]
+        testing.expect.check(expected, actual)
+
+    def createreview(commits):
+        """Create a review of the specified commits."""
+        index = len(reviews) + 1
+        branch = "033-propagation-vs-rebase/%d" % index
+        summary = "033-propagation-vs-rebase, test %d" % index
+        work.run(["push", REMOTE_URL, "%s:refs/heads/%s" % (commits[-1],
+                                                            branch)])
+        result = frontend.operation(
+            "submitreview",
+            data={ "repository": "critic",
+                   "commit_sha1s": [sha1 for sha1 in commits],
+                   "branch": "r/" + branch,
+                   "frombranch": branch,
+                   "summary": summary })
+        reviews.append({ "id": result["review_id"],
+                         "branch": "r/" + branch,
+                         "summary": summary })
+        expectmail("New Review")
+
+    def push(new_head, force=False):
+        """Push specified commit to the review branch in Critic's repository,
+           optionally forced."""
+        review = reviews[-1]
+        args = ["push"]
+        if force:
+            args.append("-f")
+        args.extend([REMOTE_URL, "%s:refs/heads/%s" % (new_head,
+                                                       review["branch"])])
+        work.run(args)
+        expecthead(new_head)
+
+    def moverebase(new_upstream, new_head):
+        """Perform a move rebase."""
+        review = reviews[-1]
+        work.run(["reset", "--hard", new_head])
+        frontend.operation(
+            "preparerebase",
+            data={ "review_id": review["id"],
+                   "new_upstream": new_upstream })
+        push(new_head, force=True)
+        expectmail("Rebased Review")
+
+    def historyrewrite(new_head):
+        """Perform a history rewrite rebase."""
+        review = reviews[-1]
+        work.run(["reset", "--hard", new_head])
+        frontend.operation(
+            "preparerebase",
+            data={ "review_id": review["id"] })
+        push(new_head, force=True)
+        expectmail("Rebased Review")
+
+    def createcomment(parent_sha1, child_sha1, offset, count, verdict):
+        frontend.operation(
+            "validatecommentchain",
+            data={ "review_id": reviews[-1]["id"],
+                   "origin": "new",
+                   "parent_sha1": parent_sha1,
+                   "child_sha1": child_sha1,
+                   "file_path": FILENAME,
+                   "offset": offset,
+                   "count": count },
+            expect={ "verdict": verdict })
+        frontend.operation(
+            "createcommentchain",
+            data={ "review_id": reviews[-1]["id"],
+                   "chain_type": "issue",
+                   "file_context": {
+                       "origin": "new",
+                       "parent_sha1": parent_sha1,
+                       "child_sha1": child_sha1,
+                       "file_path": FILENAME,
+                       "offset": offset,
+                       "count": count },
+                   "text": ("Issue at lines %d-%d"
+                            % (offset, offset + count - 1)) })
+
+    start_sha1 = work.run(["rev-parse", "HEAD"]).strip()
+
+    # TEST #1: Create a review with one commit, then do a fast-forward move
+    # rebase, and then create some comments in the diff of the original,
+    # pre-rebase commit.
+
+    work.run(["checkout", "-b", "033-propagation-vs-rebase-base", start_sha1])
+    base_commits = [commit("Base commit 1"),
+                    commit("Base commit 2", 0),
+                    commit("Base commit 3", 0, 1)]
+    work.run(["push", REMOTE_URL, "HEAD"])
+
+    work.run(["checkout", "-b", "033-propagation-vs-rebase-1", base_commits[0]])
+    original_commits = [commit("Test #1, commit 1", 6, 7)]
+    createreview(original_commits)
+    work.run(["reset", "--hard", base_commits[2]])
+    rebased_commits = [commit("Test #1, commit 1 (rebased)", 0, 1, 6, 7)]
+    moverebase(base_commits[2], rebased_commits[0])
+
+    # Lines modified in the rebase.
+    createcomment(base_commits[0], original_commits[0], 1, 2, "modified")
+
+    # Lines not modified at all.
+    createcomment(base_commits[0], original_commits[0], 4, 2, "transferred")
+
+    # Lines modified in the review (but not in the rebase).
+    createcomment(base_commits[0], original_commits[0], 7, 2, "transferred")
+
+    # TEST #2: Create a review with one commit, then do a non-fast-forward move
+    # rebase, and then create some comments in the diff of the original,
+    # pre-rebase commit.
+
+    work.run(["checkout", "-b", "033-propagation-vs-rebase-2", base_commits[2]])
+    original_commits = [commit("Test #2, commit 1", 0, 1, 6, 7)]
+    createreview(original_commits)
+    work.run(["reset", "--hard", base_commits[0]])
+    rebased_commits = [commit("Test #2, commit 1 (rebased)", 6, 7)]
+    moverebase(base_commits[0], rebased_commits[0])
+
+    # Lines modified in the rebase.
+    createcomment(base_commits[0], original_commits[0], 1, 2, "modified")
+
+    # Lines not modified at all.
+    createcomment(base_commits[0], original_commits[0], 4, 2, "transferred")
+
+    # Lines modified in the review (but not in the rebase).
+    createcomment(base_commits[0], original_commits[0], 7, 2, "transferred")